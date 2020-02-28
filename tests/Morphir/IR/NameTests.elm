ba1af08ff6eed6049eadee4eb41dc4db191ab99c
Attila Mihaly <attmih@labs.morganstanley.com>
2020-02-28 22:12:28 +0000

    Added Name tests.

---
+++ tests/Morphir/IR/NameTests.elm
@@ -0,0 +1,87 @@
+module Morphir.IR.NameTests exposing (..)
+
+import Expect
+import Morphir.IR.Name as Name
+import Test exposing (..)
+
+
+fromStringTests : Test
+fromStringTests =
+    let
+        assert inString outList =
+            test ("From string " ++ inString) <|
+                \_ ->
+                    Name.fromString inString
+                        |> Expect.equal (Name.fromList outList)
+    in
+    describe "fromString"
+        [ assert "fooBar_baz 123" [ "foo", "bar", "baz", "123" ]
+        , assert "valueInUSD" [ "value", "in", "u", "s", "d" ]
+        , assert "ValueInUSD" [ "value", "in", "u", "s", "d" ]
+        , assert "value_in_USD" [ "value", "in", "u", "s", "d" ]
+        , assert "_-% " []
+        ]
+
+
+toTitleCaseTests : Test
+toTitleCaseTests =
+    let
+        assert inList outString =
+            test ("Title case " ++ outString) <|
+                \_ ->
+                    Name.fromList inList
+                        |> Name.toTitleCase
+                        |> Expect.equal outString
+    in
+    describe "toTitleCase"
+        [ assert [ "foo", "bar", "baz", "123" ] "FooBarBaz123"
+        , assert [ "value", "in", "u", "s", "d" ] "ValueInUSD"
+        ]
+
+
+toCamelCaseTests : Test
+toCamelCaseTests =
+    let
+        assert inList outString =
+            test ("Camel case " ++ outString) <|
+                \_ ->
+                    Name.fromList inList
+                        |> Name.toCamelCase
+                        |> Expect.equal outString
+    in
+    describe "toCamelCase"
+        [ assert [ "foo", "bar", "baz", "123" ] "fooBarBaz123"
+        , assert [ "value", "in", "u", "s", "d" ] "valueInUSD"
+        ]
+
+
+toSnakeCaseTests : Test
+toSnakeCaseTests =
+    let
+        assert inList outString =
+            test ("Snake case " ++ outString) <|
+                \_ ->
+                    Name.fromList inList
+                        |> Name.toSnakeCase
+                        |> Expect.equal outString
+    in
+    describe "toSnakeCase"
+        [ assert [ "foo", "bar", "baz", "123" ] "foo_bar_baz_123"
+        , assert [ "value", "in", "u", "s", "d" ] "value_in_USD"
+        ]
+
+
+toHumanWordsTests : Test
+toHumanWordsTests =
+    let
+        assert inList outList =
+            test ("Human words " ++ (outList |> String.join " ")) <|
+                \_ ->
+                    Name.fromList inList
+                        |> Name.toHumanWords
+                        |> Expect.equal outList
+    in
+    describe "toHumanWords"
+        [ assert [ "foo", "bar", "baz", "123" ] [ "foo", "bar", "baz", "123" ]
+        , assert [ "value", "in", "u", "s", "d" ] [ "value", "in", "USD" ]
+        ]
