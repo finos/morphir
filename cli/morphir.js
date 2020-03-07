dbcc28384bbdba54ad4ffb4582b0c4dacf06358d
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-03-07 00:41:17 -0500

    CLI now loads sources from file system.

--- cli/morphir.js
+++ cli/morphir.js
@@ -1,8 +1,9 @@
 'use strict'
-const util = require('util')
 const path = require('path')
+const util = require('util')
 const fs = require('fs')
 const readdir = util.promisify(fs.readdir)
+const readFile = util.promisify(fs.readFile)
 const worker = require('./Morphir.Elm.CLI').Elm.Morphir.Elm.CLI.init()
 
 
@@ -23,27 +24,44 @@ const packageInfo = {
 
 const sourceDir = "../src"
 
-const sourceFiles = [
-    { path: "A.elm"
-    , content: "module A exposing (..)"
+readElmSources(sourceDir)
+.then((sourceFiles) => {
+    worker.ports.packageDefinitionFromSource.send([packageInfo, sourceFiles])
+    sourceFiles.forEach(element => {
+        console.log(element.path)
+    });
+})
+.catch((err) => {
+    console.error(err)
+})
+
+
+
+async function readElmSources(dir) {
+    const readElmSource = async function(filePath) {
+        const content = await readFile(filePath) 
+        return {
+            path: filePath,
+            content: content.toString()
+        }
     }
-]
+    const readDir = async function(currentDir) {
+        const entries = await readdir(currentDir, {withFileTypes: true})
+        const elmSources =
+            entries
+                .filter(entry => entry.isFile() && entry.name.endsWith('.elm'))
+                .map(entry => readElmSource(path.join(currentDir, entry.name)))
+        const subDirSources =
+            entries
+                .filter(entry => entry.isDirectory())
+                .map(entry => readDir(path.join(currentDir, entry.name)))
+                .reduce(async (soFarPromise, nextPromise) => {
+                    const soFar = await soFarPromise
+                    const next = await nextPromise
+                    return soFar.concat(next)
+                }, Promise.resolve([]))
+        return elmSources.concat(await subDirSources)        
+    }
 
-worker.ports.packageDefinitionFromSource.send([packageInfo, sourceFiles])
-
-
-async function readSourceFiles(rootDir) {
-    const entries = await readdir(rootDir, {withFileTypes: true})
-    const sourceFiles = 
-        entries
-            .filter(dirent => dirent.isFile() && dirent.name.endsWith('.elm'))
-            .map(dirent => {
-                path: path.resolve(rootDir, dirent.name)
-            })
-    const childSourceFiles = await Promise.all(
-        entries
-            .filter(dirent => dirent.isDirectory())
-            .map(dirent => readSourceFiles(path.resolve(rootDir, dirent.name)))
-    )    
-    childSourceFiles.reduce((a, b) => a.concat(b))
+    return Promise.all(await readDir(dir))
 }
