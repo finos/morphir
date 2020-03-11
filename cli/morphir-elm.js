fd717d042b4b46b38707b5cb9d9b468c91081004
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-03-11 16:45:04 -0400

    Setup for NPM publishing. #1

--- cli/morphir-elm.js
+++ cli/morphir-elm.js
@@ -1,11 +1,24 @@
 'use strict'
+// NPM imports
 const path = require('path')
 const util = require('util')
 const fs = require('fs')
 const readdir = util.promisify(fs.readdir)
 const readFile = util.promisify(fs.readFile)
+const commander = require('commander')
+
+// Elm imports
 const packageDefWorker = require('./Morphir.Elm.CLI').Elm.Morphir.Elm.CLI.init()
 const elmEncoderWorker = require('./Morphir.Elm.EncodersCLI').Elm.Morphir.Elm.EncodersCLI.init()
+
+// Read the package.json of this package
+const packageJson = require(path.join(__dirname, '../package.json'))
+
+// Set up Commander
+const program = new commander.Command()
+program
+    .version(packageJson.version, '-v, --version')
+    .parse(process.argv)
 
 
 packageDefWorker.ports.decodeError.subscribe(res => {
@@ -31,39 +44,39 @@ const sourceDir = "../src"
 const testDir = "../tests/Morphir/Codec/Tests"
 
 readElmSources(sourceDir)
-.then((sourceFiles) => {
-    packageDefWorker.ports.packageDefinitionFromSource.send([packageInfo, sourceFiles])
-    sourceFiles.forEach(element => {
-        console.log(element.path)
-    });
-})
-.catch((err) => {
-    console.error(err)
-})
+    .then((sourceFiles) => {
+        packageDefWorker.ports.packageDefinitionFromSource.send([packageInfo, sourceFiles])
+        sourceFiles.forEach(element => {
+            console.log(element.path)
+        });
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
 readElmSources(testDir)
-.then((sourceFiles) => {
-    console.log ("Generating elm encoders for following:")
-    sourceFiles.forEach(element => {
-        console.log(element.path)
-    });
-    console.log ("")
-    elmEncoderWorker.ports.elmFrontEnd.send([packageInfo, sourceFiles])
-    console.log ("")
-})
+    .then((sourceFiles) => {
+        console.log("Generating elm encoders for following:")
+        sourceFiles.forEach(element => {
+            console.log(element.path)
+        });
+        console.log("")
+        elmEncoderWorker.ports.elmFrontEnd.send([packageInfo, sourceFiles])
+        console.log("")
+    })
 
 
 
 async function readElmSources(dir) {
-    const readElmSource = async function(filePath) {
-        const content = await readFile(filePath) 
+    const readElmSource = async function (filePath) {
+        const content = await readFile(filePath)
         return {
             path: filePath,
             content: content.toString()
         }
     }
-    const readDir = async function(currentDir) {
-        const entries = await readdir(currentDir, {withFileTypes: true})
+    const readDir = async function (currentDir) {
+        const entries = await readdir(currentDir, { withFileTypes: true })
         const elmSources =
             entries
                 .filter(entry => entry.isFile() && entry.name.endsWith('.elm'))
@@ -77,7 +90,7 @@ async function readElmSources(dir) {
                     const next = await nextPromise
                     return soFar.concat(next)
                 }, Promise.resolve([]))
-        return elmSources.concat(await subDirSources)        
+        return elmSources.concat(await subDirSources)
     }
 
     return Promise.all(await readDir(dir))
