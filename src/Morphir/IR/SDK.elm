0de01be51b092611f2c28ee10bd75168adb851ab
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-03-26 16:56:03 -0400

    Added missing module declarations. #37

--- src/Morphir/IR/SDK.elm
+++ src/Morphir/IR/SDK.elm
@@ -2,13 +2,23 @@ module Morphir.IR.SDK exposing (..)
 
 import Dict
 import Morphir.IR.Advanced.Package as Package
+import Morphir.IR.SDK.Bool as Bool
+import Morphir.IR.SDK.Float as Float
 import Morphir.IR.SDK.Int as Int
+import Morphir.IR.SDK.List as List
+import Morphir.IR.SDK.Maybe as Maybe
+import Morphir.IR.SDK.String as String
 
 
 packageDeclaration : Package.Declaration ()
 packageDeclaration =
     { modules =
         Dict.fromList
-            [ ( [ [ "int" ] ], Int.moduleDeclaration )
+            [ ( [ [ "bool" ] ], Bool.moduleDeclaration )
+            , ( [ [ "int" ] ], Int.moduleDeclaration )
+            , ( [ [ "float" ] ], Float.moduleDeclaration )
+            , ( [ [ "string" ] ], String.moduleDeclaration )
+            , ( [ [ "maybe" ] ], Maybe.moduleDeclaration )
+            , ( [ [ "list" ] ], List.moduleDeclaration )
             ]
     }
--- src/Morphir/IR/SDK/Maybe.elm
+++ src/Morphir/IR/SDK/Maybe.elm
@@ -1,6 +1,8 @@
 module Morphir.IR.SDK.Maybe exposing (..)
 
-import Morphir.IR.Advanced.Type exposing (Type(..))
+import Dict
+import Morphir.IR.Advanced.Module as Module
+import Morphir.IR.Advanced.Type as Type exposing (Declaration(..), Type(..))
 import Morphir.IR.FQName as FQName exposing (FQName)
 import Morphir.IR.Name as Name
 import Morphir.IR.Path exposing (Path)
@@ -11,6 +13,22 @@ import Morphir.IR.SDK.Common exposing (packageName)
 moduleName : Path
 moduleName =
     [ [ "maybe" ] ]
+
+
+moduleDeclaration : Module.Declaration ()
+moduleDeclaration =
+    { types =
+        Dict.fromList
+            [ ( [ "maybe" ]
+              , CustomTypeDeclaration [ [ "a" ] ]
+                    [ ( [ "just" ], [ ( [ "value" ], Type.Variable [ "a" ] () ) ] )
+                    , ( [ "nothing" ], [] )
+                    ]
+              )
+            ]
+    , values =
+        Dict.empty
+    }
 
 
 fromLocalName : String -> FQName
