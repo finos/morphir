e70b0e75aa6c236a2753ade7ca620866596b2394
reesh-a <Reeshabh.Agrawal@morganstanley.com>
2020-04-14 16:47:10 -0400

    Added docker file and made shell script build only

--- examples/morphir-dapr-build-example.sh
+++ examples/morphir-dapr-build-example.sh
@@ -14,8 +14,8 @@ elm make Main.elm --output=Main.js
 sleep 2
 
 # Install and run the dapr app
-npm install
-dapr run --app-id counter --app-port 3000 --port 3500 node DaprAppShell.js
+# npm install
+# dapr run --app-id counter --app-port 3000 --port 3500 node DaprAppShell.js
 
 # Publish message to the app
 # dapr publish --topic A --payload '{"key" : "Deal1234", "command" : { "$type" : { "openDeal" : { "arg1" : "Deal1234", "arg2" : "cusip", "arg3" : 34.56, "arg4" : 100 } } } }'
