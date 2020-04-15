0ebe518c68ad034c6ca5c330e51ea2620b59ec6b
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-04-15 15:04:43 -0400

    Prepare Elm module publishing. #2

--- src/Morphir/SDK/StatefulApp.elm
+++ src/Morphir/SDK/StatefulApp.elm
@@ -1,5 +1,13 @@
-module Morphir.SDK.StatefulApp exposing (..)
+module Morphir.SDK.StatefulApp exposing (StatefulApp)
+
+{-| Utilities for modeling stateful applications.
+
+@docs StatefulApp
+
+-}
 
 
+{-| Type that represents a stateful application.
+-}
 type alias StatefulApp k c s e =
     { businessLogic : k -> Maybe s -> c -> ( k, Maybe s, e ) }
