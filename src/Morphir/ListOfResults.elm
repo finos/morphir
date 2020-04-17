7f312ba9c6bb4135d62df6b6a4465a0e2f359053
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-04-17 18:16:57 -0400

    Added utility function and removed unused one.

--- src/Morphir/ListOfResults.elm
+++ src/Morphir/ListOfResults.elm
@@ -1,35 +1,4 @@
-module Morphir.ListOfResults exposing (liftAllErrors, reduce)
-
-
-reduce : (List a -> b) -> List (Result e a) -> Result e b
-reduce f results =
-    let
-        oks =
-            results
-                |> List.filterMap
-                    (\result ->
-                        result
-                            |> Result.toMaybe
-                    )
-
-        errs =
-            results
-                |> List.filterMap
-                    (\result ->
-                        case result of
-                            Ok _ ->
-                                Nothing
-
-                            Err e ->
-                                Just e
-                    )
-    in
-    case errs of
-        [] ->
-            Ok (f oks)
-
-        firstError :: _ ->
-            Err firstError
+module Morphir.ListOfResults exposing (liftAllErrors, liftFirstError)
 
 
 liftAllErrors : List (Result e a) -> Result (List e) (List a)
@@ -61,3 +30,16 @@ liftAllErrors results =
 
         _ ->
             Err errs
+
+
+liftFirstError : List (Result e a) -> Result e (List a)
+liftFirstError results =
+    case liftAllErrors results of
+        Ok a ->
+            Ok a
+
+        Err errors ->
+            errors
+                |> List.head
+                |> Maybe.map Err
+                |> Maybe.withDefault (Ok [])
