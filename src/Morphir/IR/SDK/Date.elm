eb46ff7d191775452caf09cc13ac48660073de77
goldste <goldste@ms.com>
2020-07-08 18:27:57 -0400

    Added Date

---
+++ src/Morphir/IR/SDK/Date.elm
@@ -0,0 +1,30 @@
+module Morphir.IR.SDK.Date exposing (..)
+
+import Dict
+import Morphir.IR.Documented exposing (Documented)
+import Morphir.IR.Module as Module exposing (ModulePath)
+import Morphir.IR.Name as Name
+import Morphir.IR.Path as Path exposing (Path)
+import Morphir.IR.SDK.Common exposing (toFQName)
+import Morphir.IR.Type exposing (Specification(..), Type(..))
+
+
+moduleName : ModulePath
+moduleName =
+    Path.fromString "Date"
+
+
+moduleSpec : Module.Specification ()
+moduleSpec =
+    { types =
+        Dict.fromList
+            [ ( Name.fromString "Date", OpaqueTypeSpecification [] |> Documented "Type that represents a date concept." )
+            ]
+    , values =
+        Dict.empty
+    }
+
+
+dateType : a -> Type a
+dateType attributes =
+    Reference attributes (toFQName moduleName "Date") []
