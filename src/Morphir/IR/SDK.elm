1e954a0151e92db233a4f2ce9d6da06d4b773d72
Attila Mihaly <60483498+AttilaMihaly@users.noreply.github.com>
2020-09-08 16:47:08 -0400

    Removed key from API and added sample model with tests. #148 (#149)
    * Removed key from API and added sample model with tests. #148
    
    * Fixed resolution for StatefulApp.
    
    * Fix merge error.

--- src/Morphir/IR/SDK.elm
+++ src/Morphir/IR/SDK.elm
@@ -27,6 +27,7 @@ import Morphir.IR.SDK.List as List
 import Morphir.IR.SDK.Maybe as Maybe
 import Morphir.IR.SDK.Regex as Regex
 import Morphir.IR.SDK.Result as Result
+import Morphir.IR.SDK.StatefulApp as StatefulApp
 import Morphir.IR.SDK.String as String
 import Morphir.IR.SDK.Tuple as Tuple
 
@@ -49,5 +50,6 @@ packageSpec =
             , ( [ [ "list" ] ], List.moduleSpec )
             , ( [ [ "tuple" ] ], Tuple.moduleSpec )
             , ( [ [ "regex" ] ], Regex.moduleSpec )
+            , ( [ [ "stateful", "app" ] ], StatefulApp.moduleSpec )
             ]
     }
