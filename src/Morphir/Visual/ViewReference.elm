2140aa794213cca9bd4111d4cf8d1bf27bc8811f
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2020-12-07 15:30:45 -0500

    Insight enhancements. #220

---
+++ src/Morphir/Visual/ViewReference.elm
@@ -0,0 +1,13 @@
+module Morphir.Visual.ViewReference exposing (..)
+
+import Element exposing (Element, text)
+import Morphir.IR.FQName exposing (FQName)
+import Morphir.IR.Type exposing (Type)
+import Morphir.IR.Value exposing (Value)
+import Morphir.Visual.Common exposing (nameToText)
+
+
+view : (Value ta (Type ta) -> Element msg) -> FQName -> Element msg
+view viewValue (FQName packageName moduleName localName) =
+    text
+        (nameToText localName)
