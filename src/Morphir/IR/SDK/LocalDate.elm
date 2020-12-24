df7af35c6475a2cd85957b908f49687626910c44
Stephen Goldbaum <stephen.goldbaum@gmail.com>
2020-12-24 13:30:21 -0500

    Added LocalDate constructors.
    Issue:
    https://github.com/finos/morphir-elm/issues/239

--- src/Morphir/IR/SDK/LocalDate.elm
+++ src/Morphir/IR/SDK/LocalDate.elm
@@ -24,6 +24,8 @@ import Morphir.IR.Name as Name
 import Morphir.IR.Path as Path exposing (Path)
 import Morphir.IR.SDK.Basics exposing (intType)
 import Morphir.IR.SDK.Common exposing (toFQName, vSpec)
+import Morphir.IR.SDK.Maybe exposing (maybeType)
+import Morphir.IR.SDK.String exposing (stringType)
 import Morphir.IR.Type as Type exposing (Specification(..), Type(..))
 import Morphir.IR.Value as Value
 
@@ -41,7 +43,9 @@ moduleSpec =
             ]
     , values =
         Dict.fromList
-            [ vSpec "diffInDays" [ ( "date1", localDateType () ), ( "date2", localDateType () ) ] (intType ())
+            [ vSpec "fromISO" [ ( "iso", stringType () ) ] (maybeType () (localDateType ()))
+            , vSpec "fromParts" [ ( "year", intType () ), ( "month", intType () ), ( "day", intType () ) ] (maybeType () (localDateType ()))
+            , vSpec "diffInDays" [ ( "date1", localDateType () ), ( "date2", localDateType () ) ] (intType ())
             , vSpec "diffInWeeks" [ ( "date1", localDateType () ), ( "date2", localDateType () ) ] (intType ())
             , vSpec "diffInMonths" [ ( "date1", localDateType () ), ( "date2", localDateType () ) ] (intType ())
             , vSpec "diffInYears" [ ( "date1", localDateType () ), ( "date2", localDateType () ) ] (intType ())
--- tests/Morphir/SDK/LocalDateTests.elm
+++ tests/Morphir/SDK/LocalDateTests.elm
@@ -1,32 +1,26 @@
 module Morphir.SDK.LocalDateTests exposing (..)
 
 {-
-Copyright 2020 Morgan Stanley
+   Copyright 2020 Morgan Stanley
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+       http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 -}
 
-
+import Date as Date exposing (Unit(..), add, fromCalendarDate)
 import Expect
+import Morphir.SDK.LocalDate as LocalDate exposing (..)
 import Test exposing (..)
-
-import Morphir.SDK.LocalDate
-
-ld = 1
-
-{- 
 import Time exposing (Month(..))
-import Date exposing (Unit(..))
 
 
 mathTests : Test
@@ -34,8 +28,72 @@ mathTests =
     describe "date maths"
         [ test "add day" <|
             \_ ->
-                (LocalDate.fromCalendarDate 2020 Jan 1) 
-                    |> LocalDate.add Months 1
-                    |> Expect.equal (LocalDate.fromCalendarDate 2020 Feb 1)
+                Date.fromCalendarDate 2020 Jan 1
+                    |> LocalDate.addDays 1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Jan 2)
+        , test "subtract day" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Jan 2
+                    |> LocalDate.addDays -1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Jan 1)
+        , test "add week" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Jan 7
+                    |> LocalDate.addWeeks 1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Jan 14)
+        , test "subtract week" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Jan 14
+                    |> LocalDate.addWeeks -1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Jan 7)
+        , test "add month" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Jan 1
+                    |> LocalDate.addMonths 1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Feb 1)
+        , test "subtract month" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Feb 1
+                    |> LocalDate.addMonths -1
+                    |> Expect.equal (Date.fromCalendarDate 2020 Jan 1)
+        , test "add year" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Jan 1
+                    |> LocalDate.addYears 1
+                    |> Expect.equal (Date.fromCalendarDate 2021 Jan 1)
+        , test "subtract year" <|
+            \_ ->
+                Date.fromCalendarDate 2020 Feb 1
+                    |> LocalDate.addYears -1
+                    |> Expect.equal (Date.fromCalendarDate 2019 Feb 1)
         ]
--}
+
+
+constructorTests : Test
+constructorTests =
+    describe "constructor tests"
+        [ test "valid fromISO" <|
+            \_ ->
+                LocalDate.fromISO "2020-01-01"
+                    |> Expect.equal (Date.fromIsoString "2020-01-01" |> Result.toMaybe)
+        , test "invalid fromISO parsing" <|
+            \_ ->
+                LocalDate.fromISO "2020-01 hello"
+                    |> Expect.equal Nothing
+        , test "invalid fromISO numeric" <|
+            \_ ->
+                LocalDate.fromISO "2020-01-55"
+                    |> Expect.equal Nothing
+        , test "valid fromParts" <|
+            \_ ->
+                LocalDate.fromParts 2020 1 1
+                    |> Expect.equal (Just (Date.fromCalendarDate 2020 Jan 1))
+        , test "invalid month fromParts" <|
+            \_ ->
+                LocalDate.fromParts 2020 13 1
+                    |> Expect.equal Nothing
+        , test "invalid day fromParts" <|
+            \_ ->
+                LocalDate.fromParts 2020 2 30
+                    |> Expect.equal Nothing
+        ]
