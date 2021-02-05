afc6918c16a5c546da077afec9f75023fdc2bcee
Piyush Gupta <34481252+piyush-gupta1999@users.noreply.github.com>
2021-02-05 20:32:02 +0530

    Json Decoder Test cases started

--- src/Morphir/IR/DataCodec.elm
+++ src/Morphir/IR/DataCodec.elm
@@ -4,42 +4,42 @@ import Json.Decode as Decode
 import Morphir.IR.Distribution as Distribution exposing (Distribution)
 import Morphir.IR.FQName as FQName
 import Morphir.IR.Literal exposing (Literal(..))
+import Morphir.IR.Name as Name
 import Morphir.IR.QName exposing (QName(..))
 import Morphir.IR.Type as Type exposing (Type)
 import Morphir.IR.Value as Value exposing (Value)
 
 
-decodeData : Distribution -> Type () -> Result String (Decode.Decoder (Value () ()))
-decodeData distro tpe =
+decodeData : Type () -> Result String (Decode.Decoder (Value () ()))
+decodeData tpe =
     case tpe of
         Type.Record _ fields ->
-            Err "Wrong"
+            fields
+                |> List.foldr
+                    (\field resultSoFar ->
+                        resultSoFar
+                            |> Result.andThen
+                                (\decoderSoFar ->
+                                    decodeData field.tpe
+                                        |> Result.map
+                                            (\fieldDecoder ->
+                                                decoderSoFar
+                                                    |> Decode.andThen
+                                                        (\fieldValuesSoFar ->
+                                                            Decode.field
+                                                                (field.name |> Name.toCamelCase)
+                                                                fieldDecoder
+                                                                |> Decode.map
+                                                                    (\fieldValue ->
+                                                                        ( field.name, fieldValue ) :: fieldValuesSoFar
+                                                                    )
+                                                        )
+                                            )
+                                )
+                    )
+                    (Ok (Decode.succeed []))
+                |> Result.map (\decoder -> Decode.map (Value.Record ()) decoder)
 
-        --fields
-        --    |> List.foldr
-        --        (\field resultSoFar ->
-        --            resultSoFar
-        --                |> Result.andThen
-        --                    (\decoderSoFar ->
-        --                        decoderSoFar
-        --                            |> Decode.andThen
-        --                                (\fieldValuesSoFar ->
-        --                                    decodeData distro field.tpe
-        --                                        |> Result.map
-        --                                            (\fieldDecoder ->
-        --                                                Decode.field
-        --                                                    (field.name |> Name.toCamelCase)
-        --                                                    fieldDecoder
-        --                                                    |> Decode.map
-        --                                                        (\fieldValue ->
-        --                                                            ( field.name, fieldValue ) :: fieldValuesSoFar
-        --                                                        )
-        --                                            )
-        --                                )
-        --                    )
-        --        )
-        --        (Ok (Decode.succeed []))
-        --    |> Decode.map (Value.Record ())
         Type.Reference _ (( packageName, moduleName, localName ) as fQName) args ->
             case FQName.toString fQName of
                 "Morphir.SDK.Basics.Int" ->
@@ -51,43 +51,17 @@ decodeData distro tpe =
                 "Morphir.SDK.Basics.Bool" ->
                     Ok (Decode.map (\value -> Value.Literal () (BoolLiteral value)) Decode.bool)
 
+                "Morphir.SDK.Char.Char" ->
+                    Ok (Decode.map (\value -> Value.Literal () (StringLiteral value)) Decode.string)
+
                 "Morphir.SDK.String.String" ->
                     Ok (Decode.map (\value -> Value.Literal () (StringLiteral value)) Decode.string)
 
                 "Morphir.SDK.List.List" ->
-                    Err "Wrong"
+                    Err "Cannot Decode this type"
 
-                --Decode.list (List.head (\argValue -> decodeData distro argValue) ) args
-                --Ok (Decode.map (\argValue -> decodeData distro argValue) (Decode.list args))
                 _ ->
-                    distro
-                        |> Distribution.lookupValueDefinition (QName moduleName localName)
-                        |> Maybe.map
-                            (\valueDef ->
-                                decodeData distro valueDef.outputType
-                            )
-                        |> Maybe.withDefault (Err "Cannot Decode this type")
-
-        Type.Tuple _ listArgs ->
-            listArgs
-                |> List.foldr
-                    (\argValue decodeSoFar ->
-                        decodeSoFar
-                            |> Decode.andThen
-                                (\listValueSoFar ->
-                                    (case decodeData distro argValue of
-                                        Ok decoder ->
-                                            decoder
-
-                                        Err error ->
-                                            Decode.succeed []
-                                    )
-                                        |> Decode.map (\listValue -> listValue :: listValueSoFar)
-                                )
-                    )
-                    (Decode.succeed [])
-                |> Decode.map (Value.Tuple ())
-                |> Ok
+                    Err "Cannot Decode this type"
 
         _ ->
             Err "Cannot Decode this type"
---
+++ tests/Morphir/IR/JsonDecoderTest.elm
@@ -0,0 +1,109 @@
+module Morphir.IR.JsonDecoderTest exposing (..)
+
+import Expect
+import Json.Decode as Decode
+import Morphir.IR.DataCodec exposing (decodeData)
+import Morphir.IR.FQName exposing (fqn)
+import Morphir.IR.Literal exposing (Literal(..))
+import Morphir.IR.Type as Type exposing (Type)
+import Morphir.IR.Value as Value exposing (Value)
+import Test exposing (Test, describe, test)
+
+
+decodeDataTest : Test
+decodeDataTest =
+    let
+        intDecoder : Type ()
+        intDecoder =
+            Type.Reference () (fqn "Morphir.SDK" "Basics" "Int") []
+
+        floatDecoder : Type ()
+        floatDecoder =
+            Type.Reference () (fqn "Morphir.SDK" "Basics" "Float") []
+
+        charDecoder : Type ()
+        charDecoder =
+            Type.Reference () (fqn "Morphir.SDK" "Char" "Char") []
+
+        stringDecoder : Type ()
+        stringDecoder =
+            Type.Reference () (fqn "Morphir.SDK" "String" "String") []
+
+        jsonDecoder : Type () -> Result String (Decode.Decoder (Value () ()))
+        jsonDecoder typeDecoder =
+            decodeData typeDecoder
+    in
+    describe "JsonDecoderTest"
+        [ test "PassedInt"
+            (\_ ->
+                case decodeData intDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "42") (Ok (Value.literal () (IntLiteral 425)))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "FailedInt"
+            (\_ ->
+                case jsonDecoder floatDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "42") (Ok (Value.literal () (IntLiteral 42)))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "PassedFloat"
+            (\_ ->
+                case jsonDecoder floatDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "42.5") (Ok (Value.literal () (FloatLiteral 42.5)))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "FailedFloat"
+            (\_ ->
+                case jsonDecoder intDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "42.5") (Ok (Value.literal () (FloatLiteral 42.5)))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "PassedChar"
+            (\_ ->
+                case jsonDecoder charDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "'a'") (Ok (Value.literal () (StringLiteral "a")))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "FailedChar"
+            (\_ ->
+                case jsonDecoder charDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "'b'") (Ok (Value.literal () (StringLiteral "Hello")))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "PassedString"
+            (\_ ->
+                case jsonDecoder stringDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "\"Hello\"") (Ok (Value.literal () (StringLiteral "Hello")))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        , test "FailedString"
+            (\_ ->
+                case jsonDecoder floatDecoder of
+                    Ok decoder ->
+                        Expect.equal (Decode.decodeString decoder "\"Hello\"") (Ok (Value.literal () (StringLiteral "Hello")))
+
+                    Err error ->
+                        Expect.equal "Cannot Decode this type" error
+            )
+        ]
