5c75bfca9fc05eb401403ddab76a05b5eb335738
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2021-02-22 15:24:22 -0500

    Updated tests. #364

--- tests-integration/reference-model/src/Morphir/Reference/Model/Issues/Issue364.elm
+++ tests-integration/reference-model/src/Morphir/Reference/Model/Issues/Issue364.elm
@@ -1,6 +1,6 @@
 module Morphir.Reference.Model.Issues.Issue364 exposing (..)
 
-import Morphir.SDK.Aggregate exposing (Aggregation, aggregateMap2, byKey, maximumOf, sumOf)
+import Morphir.SDK.Aggregate exposing (Aggregation, aggregateMap, aggregateMap2, byKey, maximumOf, sumOf)
 
 
 type alias Input =
@@ -12,30 +12,9 @@ type alias Input =
 
 calculate : List Input -> List ( Input, Float )
 calculate dataSet =
-    let
-        agg2 : Aggregation Input String
-        agg2 =
-            maximumOf .value |> byKey .key2
-
-        agg1 : Aggregation Input String
-        agg1 =
-            sumOf .value |> byKey .key1
-    in
     dataSet
-        |> aggregateMap2
-            agg1
-            agg2
-            (\totalValue maxValue input ->
-                ( input, totalValue * maxValue / input.value )
-            )
-
-
-calculate2 : List Input -> List ( Input, Float )
-calculate2 dataSet =
-    dataSet
-        |> aggregateMap2
+        |> aggregateMap
             (sumOf .value |> byKey .key1)
-            (maximumOf .value |> byKey .key2)
-            (\totalValue maxValue input ->
-                ( input, totalValue * maxValue / input.value )
+            (\totalValue input ->
+                ( input, totalValue * input.value )
             )
