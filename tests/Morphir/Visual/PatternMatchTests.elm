ecd7a1bac2ce7ace4df8b2a82c417879e8e7b530
Blake Charles <blake.charles@morganstanley.com>
2021-05-05 19:00:37 +0000

    removed tests for removed functions

--- tests/Morphir/Visual/PatternMatchTests.elm
+++
@@ -1,8 +0,0 @@
-module Morphir.Visual.PatternMatchTests exposing (..)
-
-import Element exposing (Attribute, Column, Element, fill, row, spacing, table, text, width)
-import Expect
-import Morphir.IR.Literal exposing (Literal(..))
-import Morphir.IR.Type as Type
-import Morphir.IR.Value as Value exposing (Pattern(..), Value(..))
-import Test exposing (..)
