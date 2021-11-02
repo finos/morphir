cd0cac627100bfd376a43576bd746535b381344c
nunlecar <Cara.Nunley@morganstanley.com>
2021-11-02 19:34:12 +0000

    update for tree and viewTree

--- src/Morphir/Visual/ViewMultiaryDecisionTree.elm
+++ src/Morphir/Visual/ViewMultiaryDecisionTree.elm
@@ -1,5 +1,5 @@
---module Morphir.Visual.ViewIfThenElseNEW exposing (view)
---
+module Morphir.Visual.ViewMultiaryDecisionTree exposing (main)
+
 --import Dict exposing (Dict)
 --import Element exposing (Element)
 --import Morphir.IR.Literal exposing (Literal(..))
@@ -10,10 +10,135 @@
 --import Morphir.Visual.VisualTypedValue exposing (VisualTypedValue)
 --
 --
+import Browser
+import Morphir.IR.FQName as FQName
+import Morphir.IR.Literal exposing (Literal(..))
+import Morphir.IR.Value as Value exposing (Pattern, RawValue, Value)
+import Html.Styled.Attributes exposing (css)
+
+import Html.Styled exposing (Html, div, fromUnstyled, map, toUnstyled)
+import Css exposing (auto, px, width)
+import Morphir.Visual.ViewPattern as ViewPattern
+
+import Morphir.IR.Value as Value
+import Tree as Tree
+import TreeView as TreeView
+
+import Morphir.IR.Value as Value exposing (Pattern, RawValue, Value)
+import Morphir.IR.Value exposing (RawValue)
+import Mwc.Button
+import Mwc.TextField
+import Element exposing (Element)
+import Morphir.Visual.Components.MultiaryDecisionTree as MultiaryDecisionTree exposing (Model, Msg(..), NodeData, configuration, expandAllCollapseAllButtons, selectedNodeDetails)
+import Morphir.Visual.Config exposing (Config)
+import Morphir.Visual.VisualTypedValue exposing (VisualTypedValue)
 --view : Config msg -> (VisualTypedValue -> Element msg) -> VisualTypedValue -> Element msg
 --view config viewValue value =
---     MultiaryDecisionTree.layout config viewValue (valueToTree config True value)
+--     MultiaryDecisionTree.main
+
+
+initialModel : () -> (Model, Cmd Msg)
+initialModel () =
+    let
+        rootNodes =
+            [ Tree.Node
+                { children =
+                    [ Tree.Node { children = [], data = NodeData "1.1" "Yes" (Just (Value.LiteralPattern () (BoolLiteral True)))}
+                    , Tree.Node { children = [], data = NodeData "1.2" "No" (Just (Value.LiteralPattern () (BoolLiteral False)))} ],
+                    data = NodeData "1" "isFoo" Nothing
+                }
+            , Tree.Node
+                { children =
+                    [ Tree.Node
+                    {
+                    children = [ Tree.Node
+                                { children = [], data = NodeData "2.1.1" "YesAndYes"  (Just( Value.LiteralPattern () (BoolLiteral True))) },
+                                Tree.Node
+                                { children = [], data = NodeData "2.1.2" "YesAndNo"  (Just( Value.LiteralPattern () (BoolLiteral True)))} ]
+                    ,
+                    data = NodeData "2.1" "isBar"  (Just( Value.LiteralPattern () (BoolLiteral True)))},
+                     Tree.Node
+                     {
+                     children = [],
+                     data = NodeData "2.2" "No"  (Just ( Value.LiteralPattern () (BoolLiteral False)))}
+                     ]
+
+                , data = NodeData "2" "isFoo" Nothing
+                }
+              , Tree.Node
+                { children = [
+                Tree.Node {
+                    children = [],
+                    data = NodeData "3.1" "foo" (Just ( Value.ConstructorPattern () (FQName.fromString "My:Sample:EnumValue1" ":") []))
+                    },
+                Tree.Node {
+                    children = [],
+                    data = NodeData "3.2" "bar" (Just ( Value.ConstructorPattern () (FQName.fromString "My:Sample:EnumValue2" ":") []))
+                    },
+                Tree.Node {
+                    children = [],
+                    data = NodeData "3.3" "baz" (Just ( Value.WildcardPattern ()))
+                    }
+                ],
+                data = NodeData "3" "enum" Nothing
+
+                }
+            ]
+    in
+        ( { rootNodes = rootNodes
+        , treeModel = TreeView.initializeModel configuration rootNodes
+        , selectedNode = Nothing
+        }
+        , Cmd.none
+        )
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update message model =
+    let
+        treeModel =
+            case message of
+                TreeViewMsg tvMsg ->
+                    TreeView.update tvMsg model.treeModel
+                ExpandAll ->
+                    TreeView.expandAll model.treeModel
+                CollapseAll ->
+                    TreeView.collapseAll model.treeModel
+    in
+        ( { model
+        | treeModel = treeModel
+        , selectedNode = TreeView.getSelected treeModel |> Maybe.map .node |> Maybe.map Tree.dataOf
+        }, Cmd.none )
+
+
+-- if (or when) you want the tree view to navigate up/down between visible nodes and expand/collapse nodes on arrow key presse
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.map TreeViewMsg (TreeView.subscriptions model.treeModel)
+
+-- avilitiy to view tree
+view : Model -> Html Msg
+view model =
+        div
+            [ css [width (auto)]]
+            [ expandAllCollapseAllButtons
+            ,selectedNodeDetails model
+            , map TreeViewMsg (TreeView.view model.treeModel |> fromUnstyled)
+            ]
+main =
+    Element.layout
+
 --
+--display : config
+--
+--
+--main =
+--    Browser.element
+--        {
+--        init = initialModel,
+--        view = view >> toUnstyled,
+--        update = update,
+--        subscriptions = subscriptions
+--        }
 --
 --valueToTree : Config msg -> Bool -> VisualTypedValue -> MultiaryDecisionTree.Node
 --valueToTree config doEval value =
