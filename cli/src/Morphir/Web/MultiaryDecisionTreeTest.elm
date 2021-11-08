2258f484ec8a910d4dca108f23f1af96e3ee3eac
seliga <robert.seliga@morganstanley.com>
2021-11-08 14:03:46 +0000

    moved some stuff around and got dropdowns working

--- cli/src/Morphir/Web/MultiaryDecisionTreeTest.elm
+++ cli/src/Morphir/Web/MultiaryDecisionTreeTest.elm
@@ -1,15 +1,19 @@
 module Morphir.Web.MultiaryDecisionTreeTest exposing (..)
 
 import Browser
+--import Html.Styled.Events as Events
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
 import Morphir.IR.Value as Value exposing (Pattern, RawValue, Value(..), toString, unit, variable)
 
 import Dict
-import Element exposing (Element, column, el, fill, html, layout, none, padding, paddingEach, px, row, shrink, spacing, table)
-import Html.Styled.Attributes exposing (class, css, id, placeholder, value)
-import Html exposing (a)
+import Element exposing (Color, Element, column, el, fill, html, layout, mouseOver, none, padding, paddingEach, px, rgb, row, shrink, spacing, table, text)
+--import Html.Styled.Attributes exposing (class, css, id, placeholder, value)
+import Html exposing (Html, map)
 
 
-import Html.Styled exposing (Html, div, fromUnstyled, map, option, select, text, toUnstyled)
+--import Html.Styled exposing (Html, button, div, fromUnstyled, map, option, select, text, toUnstyled)
 
 import Css exposing (auto, px, width)
 import Morphir.IR.Distribution exposing (Distribution(..))
@@ -32,6 +36,8 @@ import TreeView as TreeView
 import Mwc.Button
 import Mwc.TextField
 
+t = Element.text
+--text = Html.Styled.text
 --load ir and figure out what to show
 
 -- reading an ir and selecting the function in it to visualize
@@ -125,6 +131,8 @@ initialModel () =
         ( { rootNodes = rootNodes
         , treeModel = TreeView.initializeModel configuration rootNodes
         , selectedNode = Nothing
+        , favouriteFood = Normal Nothing
+        , favouriteExample = Normal Nothing
         }
         , Cmd.none
         )
@@ -134,93 +142,157 @@ type alias Model =
     { rootNodes : List (Tree.Node NodeData)
     , treeModel : TreeView.Model NodeData String Never ()
     , selectedNode : Maybe NodeData
+    , favouriteFood: Dropdown Food
+    , favouriteExample : Dropdown Example
     }
-
---construct a configuration for your tree view
-configuration : TreeView.Configuration NodeData String
-configuration =
-    TreeView.Configuration nodeUid nodeLabel TreeView.defaultCssClasses
 
 -- otherwise interact with your tree view in the usual TEA manner
 type Msg =
   TreeViewMsg (TreeView.Msg String)
   | ExpandAll
   | CollapseAll
+  | FoodDropdown (DropdownAction Food)
+  | ExampleDropdown (DropdownAction Example)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update message model =
-    let
-        treeModel =
-            case message of
-                TreeViewMsg tvMsg ->
-                    TreeView.update tvMsg model.treeModel
-                ExpandAll ->
-                    TreeView.expandAll model.treeModel
-                CollapseAll ->
-                    TreeView.collapseAll model.treeModel
-    in
-        ( { model
-        | treeModel = treeModel
-        , selectedNode = TreeView.getSelected treeModel |> Maybe.map .node |> Maybe.map Tree.dataOf
-        }, Cmd.none )
+    case message of
+        FoodDropdown action ->
+            case action of
+                OpenList ->
+                    ({model | favouriteFood  = Select foodList}, Cmd.none)
+                ClickedOption food ->
+                    ({model | favouriteFood = Normal (Just food)}, Cmd.none)
+        ExampleDropdown action ->
+            case action of
+                OpenList ->
+                    ({ model | favouriteExample = Select exampleList}, Cmd.none)
+                ClickedOption example ->
+                    ({model | favouriteExample = Normal (Just example)}, Cmd.none)
+        _ ->
+            let
 
-expandAllCollapseAllButtons : Html Msg
-expandAllCollapseAllButtons =
-    div
-      []
-      [ Mwc.Button.view
-          [ Mwc.Button.raised
-          , Mwc.Button.onClick ExpandAll
-          , Mwc.Button.label "Expand all"
-          ]
-      , Mwc.Button.view
-          [ Mwc.Button.raised
-          , Mwc.Button.onClick CollapseAll
-          , Mwc.Button.label "Collapse all"
-          ]
-      ]
+                treeModel =
+                    case message of
+                        TreeViewMsg tvMsg ->
+                            TreeView.update tvMsg model.treeModel
+                        ExpandAll ->
+                            TreeView.expandAll model.treeModel
+                        CollapseAll ->
+                            TreeView.collapseAll model.treeModel
+                        --   V this should never be hit V
+                        _ ->
+                            TreeView.expandAll model.treeModel
+            in
+                ( { model
+                | treeModel = treeModel
+                , selectedNode = TreeView.getSelected treeModel |> Maybe.map .node |> Maybe.map Tree.dataOf
+                }, Cmd.none )
 
-selectedNodeDetails : Model -> Html Msg
-selectedNodeDetails model =
+type Dropdown a
+    = Normal (Maybe a)
+    | Select (List a)
+
+type DropdownAction a
+    = OpenList
+    | ClickedOption a
+
+type alias Food =
+    { name : String}
+
+type alias Example =
+    {name : String}
+
+foodList : List Food
+foodList =
+    [ Food "Cash"
+    , Food "Inventory"
+    , Food "Pending Trades"
+    ]
+
+exampleList : List Example
+exampleList =
+    [ Example "option 1"
+    , Example "option 2"
+    , Example "option 3"
+    ]
+
+
+overColor : Color
+overColor = rgb 0.9 0.9 0.1
+
+white : Color
+white = rgb 1 1 1
+
+dropdownView : Dropdown a -> (a -> String) -> (DropdownAction a -> Msg) -> Element Msg
+dropdownView dropdownState toString toMsg =
     let
-        selectedDetails =
-            Maybe.map (\nodeData -> nodeData.uid ++ ": " ++ nodeData.subject) model.selectedNode
-                |> Maybe.withDefault "(nothing selected)"
+        selectedName =
+            case dropdownState of
+                Normal (Just someA) -> toString someA
+                _ -> "Click to select"
+        menu : Element Msg
+        menu =
+            case dropdownState of
+                Select options ->
+                    let
+                        mouseOverColor : Color
+                        mouseOverColor = rgb 0.9 0.9 0.1
+
+                        backgroundColor : Color
+                        backgroundColor = rgb 1 1 1
+
+                        viewOption : a -> Element Msg
+                        viewOption option =
+                            el
+                                [ Element.width Element.fill
+                                , Element.mouseOver [Background.color overColor]
+                                , Background.color white
+                                , Events.onClick (toMsg (ClickedOption option))
+                                ]
+                                (t <| toString option)
+
+                        viewOptionList : List a -> Element Msg
+                        viewOptionList inputOptions =
+                            column [] <|
+                                List.map viewOption inputOptions
+
+                    in
+                       el
+                            [ Border.width 1
+                            , Border.dashed
+                        , padding 3
+                        , Element.below (viewOptionList options)
+                        ]
+                        (t selectedName)
+                _ ->
+                    el
+                        [ Border.width 1
+                        , Border.dashed
+                        , padding 3
+                        , Events.onClick (toMsg OpenList)
+                        ]
+                        (t selectedName)
     in
-        div
-            [ css [ width (auto) ] ]
-            [ Mwc.TextField.view
-                [ Mwc.TextField.readonly True
-                , Mwc.TextField.label selectedDetails
-                ]
-            ]
+        menu
+
+
 
 -- avilitiy to view tree
-view : Model -> Html Msg
+view : Model -> Html.Html Msg
 view model =
-        div
-            [ css [width (auto)]]
-            [
-            select [id "first"] [option [value "0"] [text "Cash"], option [value "1"] [text "Inventory"], option [value "2"] [text "Pending Trades"]]
-            , select [id "bank-type", class "sub-selector"] [option [value "0"] [text "Central Bank"], option [value "1"] [text "Onshore"]]
-            , select [id "cash-type", class "sub-selector"] [option [value "0"] [text "Segregated Cash"], option [value "1"] [text "Not"]]
-            , select [id "negative-type", class "hidden-on-start sub-selector"] [option [value "0"] [text "NetUSD is Negative"], option [value "1"] [text "NetUSD is Positive"]]
-            --, select [id "classify-type"] [option [] [text "Classify by Counter-Party ID"], option [] [text "Don't"]]
-            --, select [id "bottom-level"] [option [] [text "FRD"], option [] [text "BOE"], option [] [text "SNB"]
-            --    , option [] [text "ECB"], option [] [text "BOJ"], option [] [text "RBA"]
-            --    , option [] [text "BOC"], option [] [text "Others"]]
-            , expandAllCollapseAllButtons
-            , selectedNodeDetails model
-            , map TreeViewMsg (TreeView.view model.treeModel |> fromUnstyled)
 
+        --layout [] dropdown
+        Html.div
+            [ ]
+            [ layout [] (dropdownView model.favouriteFood .name FoodDropdown)
+            , layout [] (dropdownView model.favouriteExample .name ExampleDropdown)
+            , map TreeViewMsg (TreeView.view model.treeModel)
             ]
 
 
--- if (or when) you want the tree view to navigate up/down between visible nodes and expand/collapse nodes on arrow key presse
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.map TreeViewMsg (TreeView.subscriptions model.treeModel)
+
 
 
 --
@@ -228,10 +300,21 @@ main =
     Browser.element
         {
         init = initialModel,
-        view = view >> toUnstyled,
+        view = view,
         update = update,
         subscriptions = subscriptions
         }
+
+--construct a configuration for your tree view
+configuration : TreeView.Configuration NodeData String
+configuration =
+    TreeView.Configuration nodeUid nodeLabel TreeView.defaultCssClasses
+
+-- if (or when) you want the tree view to navigate up/down between visible nodes and expand/collapse nodes on arrow key presse
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.map TreeViewMsg (TreeView.subscriptions model.treeModel)
+
 toMaybeList : List(Pattern(), Value () () ) -> List(Maybe(Pattern()), Value () () )
 toMaybeList list =
     let
@@ -298,66 +381,3 @@ createUIDS range currentUID =
 
     in
         List.map appender stringRange
-----
---main =
---    layout
---            []
---            <|
---            (el [ padding 10 ]
-
-                --(Element.text (Value.toString (Value.IfThenElse () (Value.Variable () ["isFoo"]) (Value.Variable () ["Yes"])
-                --(Value.IfThenElse () (Value.Variable () ["isFoo"]) (Value.Variable () ["Yes"]) (Value.Variable () ["No"]))
-                --
-                -- (Element.text
-                --    (translation
-                --        (Value.IfThenElse () (Value.Variable () ["isFoo"]) (Value.Variable () ["Yes"])(Value.Variable () ["No"])) "1"
-                --    )
-                -- )
-                --(Element.text
-                --    (translation
-                --        (Value.IfThenElse () (Value.Variable () ["isFoo"]) (Value.Variable () ["Yes1"]) (Value.IfThenElse () (Value.Variable () ["isBar"]) (Value.Variable () ["Yes2"]) (Value.Variable () ["No"]))) 1
-                --    )
-                --)
-                --(Element.text
-                --    (translation2
-                --        (Value.IfThenElse () (Value.Variable () ["isFoo"]) (Value.Variable () ["Yes1"]) (Value.IfThenElse () (Value.Variable () ["isBar"]) (Value.Variable () ["Yes2"]) (Value.Variable () ["No"]))) 1
-                --    )
-                --)
-                --(Element.text
-                --    (translation2
-                --    -- ( Value.ConstructorPattern () (FQName.fromString "My:Sample:EnumValue1" ":") []
-                --        (Nothing, (Value.patternMatch () (Value.Variable () ["LALALA"])
-                --              [
-                --              ( Value.ConstructorPattern () (FQName.fromString "My:Sample:EnumValue1" ":") [], (Value.Variable () ["BAR"]))
-                --                , ( Value.ConstructorPattern () (FQName.fromString "My:Sample:EnumValue2" ":") [], (Value.Variable () ["CAAR"]))
-                --                , ( Value.WildcardPattern (),  (Value.Variable () ["DAR"]))
-                --              ]))
-                --        1
-                --    )
-                --)
-
-
-
-
-
-
---getLabel: Maybe (Pattern ()) -> String
---
---getLabel pattern =
---    Element.table []
---            { data = pattern
---            , columns =
---                [ { header = none
---                  , width = px 150
---                  , view =
---                        \myLabel ->
---                            ViewPattern.patternAsText(pattern)
---                  }
---                , { header = none
---                  , width = px 150
---                  , view =
---                        \myLabel ->
---                            "  ->  "
---                  }
---                ]
---            }
