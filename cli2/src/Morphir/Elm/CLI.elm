b47f245e4e3ff91124e4f9e46ecd26c5104a4b93
unknown <nunya.klah@morganstanley.com>
2022-02-16 08:40:50 +0000

    create incremental build method

--- cli2/src/Morphir/Elm/CLI.elm
+++ cli2/src/Morphir/Elm/CLI.elm
@@ -1,12 +1,29 @@
-port module Morphir.Elm.CLI2 exposing (..)
+{-
+   Copyright 2020 Morgan Stanley
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
+
+port module Morphir.Elm.CLI exposing (..)
 
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Morphir.Compiler as Compiler
 import Morphir.Elm.Frontend as Frontend exposing (PackageInfo, SourceFile, SourceLocation)
 import Morphir.Elm.Frontend.Codec as FrontendCodec
-
-
-port packageDefinitionFromSource : (( Decode.Value, Decode.Value, List SourceFile ) -> msg) -> Sub msg
+import Morphir.IR as IR exposing (IR)
+import Morphir.IR.Package as Package exposing (PackageName)
 
 
 port packageDefinitionFromSourceResult : Encode.Value -> Cmd msg
@@ -15,8 +32,14 @@ port packageDefinitionFromSourceResult : Encode.Value -> Cmd msg
 port jsonDecodeError : String -> Cmd msg
 
 
+port incrementalBuild : (( Decode.Value, Decode.Value, List SourceFile, List IR ) -> msg) -> Sub msg
+
+
+port incrementalBuildResult : ( Encode.Value, Encode.Value ) -> Cmd msg
+
+
 type Msg
-    = PackageDefinitionFromSource ( Decode.Value, Decode.Value, List SourceFile )
+    = IncrementalBuild ( Decode.Value, Decode.Value, List SourceFile, List IR )
 
 
 main : Platform.Program () () Msg
@@ -28,24 +51,51 @@ main =
         }
 
 
-update : Msg -> () -> ( (), Cmd msg )
+update : Msg -> () -> ( (), Cmd Msg )
 update msg model =
     case msg of
-        PackageDefinitionFromSource ( optionsJson, packageInfoJson, sourceFiles ) ->
+        IncrementalBuild ( optionsJson, packageInfoJson, sourceFiles, irJson ) ->
             let
                 inputResult : Result Decode.Error ( Frontend.Options, PackageInfo )
                 inputResult =
                     Result.map2 Tuple.pair
-                       (Decode.decodeValue FrontendCodec.decodeOptions optionsJson)
-                       (Decode.decodeValue FrontendCodec.decodePackageInfo packageInfoJson)
+                        (Decode.decodeValue FrontendCodec.decodeOptions optionsJson)
+                        (Decode.decodeValue FrontendCodec.decodePackageInfo packageInfoJson)
+
+                -- (Decode.decodeValue FrontendCodec.decodePackageInfo packageInfoJson)
             in
-                    case inputResult of
-                        Ok ( opts, packageInfo ) ->
-                                --create empty repo and send files incrementally
-                        Err errorMessage ->
-                                -- print out error message
+            case inputResult of
+                Ok ( opts, packageInfo ) ->
+                    let
+                        createEmptyRepo : Result (List Compiler.Error) (Package.Definition () () )
+                        createEmptyRepo =
+                            Repo.Empty
+
+
+                Err errorMessage ->
+                    ( model
+                    , errorMessage
+                        |> Decode.errorToString
+                        |> jsonDecodeError
+                    )
 
 
 subscriptions : () -> Sub Msg
 subscriptions _ =
-    packageDefinitionFromSource PackageDefinitionFromSource
+    incrementalBuild IncrementalBuild
+
+
+encodeResult : (e -> Encode.Value) -> (a -> Encode.Value) -> Result e a -> Encode.Value
+encodeResult encodeErr encodeValue result =
+    case result of
+        Ok a ->
+            Encode.list identity
+                [ Encode.null
+                , encodeValue a
+                ]
+
+        Err e ->
+            Encode.list identity
+                [ encodeErr e
+                , Encode.null
+                ]
