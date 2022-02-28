15229117f7aba38f52a4f99d33597a6eae09ec72
Elorm-Axolu <Elorm.Axolu@MorganStanley.com>
2022-02-28 08:08:44 +0000

    changes request

--- cli2/morphir.ts
+++ cli2/morphir.ts
@@ -4,12 +4,11 @@ import path from 'path'
 import {Command} from 'commander'
 
 // Read the package.json of this package
-const packageJson = require(path.join(__dirname, './package.json'))
+const packageJson = require(path.join(__dirname, './../package.json'))
 
 // Set up Commander
 const program = new Command()
 program
     .version(packageJson.version, '-v, --version')
-    .command('elm', 'Invoke Elm tooling')
-    .command('dapr', 'Invoke Dapr tooling')
+    .command('make', 'Translate Elm sources to Morphir IR')
     .parse(process.argv)
