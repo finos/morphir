da3f032960403aaacda9c05cd1529347a0059c3c
Kindson The Genius <35315234+KindsonTheGenius@users.noreply.github.com>
2022-06-08 14:36:57 +0000

    Wrote a test case for genEncodeReference

--- tests/Morphir/Scala/Feature/CodecTests.elm
+++ tests/Morphir/Scala/Feature/CodecTests.elm
@@ -11,26 +11,26 @@ import Test exposing (Test, describe, test)
 genEncodeReferenceTests : Test
 genEncodeReferenceTests =
     let
-        postiveTest name inputData outputData =
+        positiveTest name input expectedOutput =
             test name
                 (\_ ->
-                    case genEncodeReference inputData of
+                    case genEncodeReference input of
                         Ok output ->
                             output
-                                |> Expect.equal outputData
+                                |> Expect.equal expectedOutput
 
                         Err error ->
                             Expect.fail error
                 )
     in
     describe "Generate Encoder Reference Tests"
-        [ postiveTest "1. Type Variable "
+        [ positiveTest "1. Type Variable "
             (Type.Variable () [ "foo" ])
             (Scala.Variable "encodeFoo")
-        , postiveTest "2. Type Reference"
+        , positiveTest "2. Type Reference"
             (Type.Reference () (fqn "morphir" "sdk" "string") [])
             (Scala.Ref [ "morphir", "sdk" ] "encodeString")
-        , postiveTest "3. Type Record with two fields"
+        , positiveTest "3. Type Record with two fields"
             (Type.Record ()
                 [ Type.Field [ "name" ] (Type.Reference () (fqn "morphir.sdk" "Basics" "String") [])
                 , Type.Field [ "age" ] (Type.Reference () (fqn "morphir.sdk" "Basics" "Int") [])
@@ -41,6 +41,34 @@ genEncodeReferenceTests =
                     (Scala.Tuple
                         [ Scala.Literal (Scala.StringLit "name")
                         , Scala.Apply (Scala.Ref [ "morphir", "sdk", "basics" ] "encodeString") [ ArgValue Nothing (Select (Variable "a") "name") ]
+                        ]
+                    )
+                , Scala.ArgValue Nothing
+                    (Scala.Tuple
+                        [ Scala.Literal (Scala.StringLit "age")
+                        , Scala.Apply (Scala.Ref [ "morphir", "sdk", "basics" ] "encodeInt") [ ArgValue Nothing (Select (Variable "a") "age") ]
+                        ]
+                    )
+                ]
+            )
+        , positiveTest "4. Type Record with three fields"
+            (Type.Record ()
+                [ Type.Field [ "firstname" ] (Type.Reference () (fqn "morphir.sdk" "Basics" "String") [])
+                , Type.Field [ "lastname" ] (Type.Reference () (fqn "morphir.sdk" "Basics" "String") [])
+                , Type.Field [ "age" ] (Type.Reference () (fqn "morphir.sdk" "Basics" "Int") [])
+                ]
+            )
+            (Scala.Apply (Scala.Ref [ "io", "circe", "Json" ] "obj")
+                [ Scala.ArgValue Nothing
+                    (Scala.Tuple
+                        [ Scala.Literal (Scala.StringLit "firstname")
+                        , Scala.Apply (Scala.Ref [ "morphir", "sdk", "basics" ] "encodeString") [ ArgValue Nothing (Select (Variable "a") "firstname") ]
+                        ]
+                    )
+                , Scala.ArgValue Nothing
+                    (Scala.Tuple
+                        [ Scala.Literal (Scala.StringLit "lastname")
+                        , Scala.Apply (Scala.Ref [ "morphir", "sdk", "basics" ] "encodeString") [ ArgValue Nothing (Select (Variable "a") "lastname") ]
                         ]
                     )
                 , Scala.ArgValue Nothing
