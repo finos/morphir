032a2a9a020e0e1da215929eaac1112fd1f46c75
klannunya <nunya.klah@morganstanley.com>
2022-06-13 11:35:35 +0000

    Add Json Schema Options to Target

--- cli/src/Morphir/Elm/Target.elm
+++ cli/src/Morphir/Elm/Target.elm
@@ -7,6 +7,7 @@ import Morphir.Graph.CypherBackend as Cypher
 import Morphir.Graph.SemanticBackend as SemanticBackend
 import Morphir.IR.Distribution exposing (Distribution)
 import Morphir.IR.Package as Package
+import Morphir.JsonSchema.Backend
 import Morphir.Scala.Backend
 import Morphir.Scala.Backend.Codec
 import Morphir.Scala.Spark.Backend
@@ -28,6 +29,7 @@ type BackendOptions
     | CypherOptions Cypher.Options
     | TypeScriptOptions Morphir.TypeScript.Backend.Options
     | SparkOptions Morphir.Scala.Spark.Backend.Options
+    | JsonSchemaOptions Morphir.JsonSchema.Backend.Options
 
 
 decodeOptions : Result Error String -> Decode.Decoder BackendOptions
@@ -47,6 +49,9 @@ decodeOptions gen =
 
         Ok "Spark" ->
             Decode.map SparkOptions (Decode.succeed Morphir.Scala.Spark.Backend.Options)
+
+        Ok "JsonSchema" ->
+            Decode.map JsonSchemaOptions (Decode.succeed Morphir.JsonSchema.Backend.Options)
 
         _ ->
             Decode.map (\options -> ScalaOptions options) Morphir.Scala.Backend.Codec.decodeOptions
@@ -72,3 +77,6 @@ mapDistribution back dist =
 
         SparkOptions options ->
             Morphir.Spark.Backend.mapDistribution options dist
+
+        JsonSchemaOptions options ->
+            Morphir.JsonSchema.Backend.mapDistribution options dist
