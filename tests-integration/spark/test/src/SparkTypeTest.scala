f9975284b91826fbcf5fa7f10919d4780020afde
Jonathan Maw <jonathan.maw@codethink.co.uk>
2022-06-17 16:45:11 +0100

    Add tests for checking against Nothing

--- tests-integration/spark/test/src/SparkTypeTest.scala
+++ tests-integration/spark/test/src/SparkTypeTest.scala
@@ -2,6 +2,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.explode
 import org.scalatest.FunSuite
 import sparktests.typetests.SparkJobs
+import org.apache.spark.sql.types.{BooleanType, StringType, StructField, StructType}
+import org.apache.spark.sql.Row
 
 class SparkTypeTest extends FunSuite {
   val localTestSession =
@@ -39,6 +41,32 @@ class SparkTypeTest extends FunSuite {
     val result = SparkJobs.testInt(df)
     assert(result.count() == 1)
     assert(result.collect()(0)(0) == good_value)
+  }
+
+  test("testMaybeBoolConditonalNull") {
+    val data = Seq(Row(null, "baz"), Row(true, "baz"))
+    val schema = StructType(List(
+      StructField("foo", BooleanType, true),
+      StructField("bar", StringType, false),
+    ))
+    val rdd = localTestSession.sparkContext.parallelize(data)
+    val df = localTestSession.createDataFrame(rdd, schema)
+    val result = SparkJobs.testMaybeBoolConditionalNull(df)
+    assert(result.count() == 1)
+    assert(result.collect()(0)(0) == null)
+  }
+
+  test("testMaybeBoolConditonalNotNull") {
+    val data = Seq(Row(null, "baz"), Row(true, "baz"))
+    val schema = StructType(List(
+      StructField("foo", BooleanType, true),
+      StructField("bar", StringType, false),
+    ))
+    val rdd = localTestSession.sparkContext.parallelize(data)
+    val df = localTestSession.createDataFrame(rdd, schema)
+    val result = SparkJobs.testMaybeBoolConditionalNotNull(df)
+    assert(result.count() == 1)
+    assert(result.collect()(0)(0) == true)
   }
 
   test("testString") {
