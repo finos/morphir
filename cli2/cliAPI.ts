495ba02644f089a2d13e3a462e594aaddf3e0ed4
Osborne Yaw Sapaty <osborne.sapaty@morganstanley.com>
2022-08-25 03:36:27 +0000

    morphir dockerize command implemented

--- cli2/cliAPI.ts
+++ cli2/cliAPI.ts
@@ -28,3 +28,22 @@ export function make(dir: string, opts: any) {
         process.exit(1)
     })
 }
+
+export function dockerize(dir: string, opts: any) {
+    cli.writeDockerfile(dir, opts)
+    .then(() => {
+        console.log("Dockerfile Created Successfully");
+    })
+    .catch((err: { code: string; path: any }) => {
+        if (err.code == 'ENOENT') {
+            console.error(`Could not find file at '${err.path}'`)
+        } else {
+            if (err instanceof Error) {
+                console.error(err)
+            } else {
+                console.error(`Error: ${JSON.stringify(err, null, 2)}`)
+            }
+        }
+        process.exit(1)
+    })
+}
