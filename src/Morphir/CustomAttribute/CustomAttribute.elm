deb9dd9e73b81fc46fc7966333cbb2a474292b71
Elorm-Axolu <Elorm.Axolu@MorganStanley.com>
2022-09-01 15:32:18 +0000

    fix build fail

--- src/Morphir/CustomAttribute/CustomAttribute.elm
+++ src/Morphir/CustomAttribute/CustomAttribute.elm
@@ -1,22 +1,27 @@
 module Morphir.CustomAttribute.CustomAttribute exposing (..)
-import Morphir.Compiler exposing (FilePath)
+
 import Dict exposing (Dict)
 import Json.Encode as Encode
+import Morphir.Compiler exposing (FilePath)
 import Morphir.IR.FQName exposing (FQName)
 import Morphir.IR.NodeId exposing (NodeID)
 
 
-type alias CustomAttributeId = String
+type alias CustomAttributeId =
+    String
+
 
 type alias CustomAttributeConfig =
-	{ filePath : FilePath
-	}
+    { filePath : FilePath }
+
 
 type alias CustomAttributeConfigs =
-	Dict CustomAttributeId CustomAttributeConfig
+    Dict CustomAttributeId CustomAttributeConfig
+
 
 type alias CustomAttributeValues =
-	Dict NodeID Encode.Value
+    Dict NodeID Encode.Value
+
 
 type alias CustomAttribute =
     Dict FQName (Dict String Encode.Value)
