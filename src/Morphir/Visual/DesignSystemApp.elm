08c32bec37e1c7b57b3bd0b77cee31a30e17278e
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2022-10-18 12:28:32 +0200

    Integrate other components into sample page.

--- src/Morphir/Visual/DesignSystemApp.elm
+++ src/Morphir/Visual/DesignSystemApp.elm
@@ -2,11 +2,15 @@ module Morphir.Visual.DesignSystemApp exposing (..)
 
 import Array exposing (Array)
 import Browser
-import Element exposing (Element, column, el, htmlAttribute, layout, padding, spacing, text)
+import Element exposing (Element, column, el, htmlAttribute, layout, none, padding, paddingEach, rgb, row, spacing, text)
+import Element.Background as Background
 import Element.Font as Font
 import FontAwesome.Styles as Icon
 import Html exposing (Html)
+import Morphir.Visual.Components.DrillDownPanel as DrillDownPanel
 import Morphir.Visual.Components.Picklist as Picklist
+import Morphir.Visual.Components.TabsComponent as TabsComponent
+import Morphir.Visual.Theme as Theme exposing (Theme)
 
 
 main : Program () Model Msg
@@ -23,13 +27,19 @@ type alias Model =
 
 
 type alias Components =
-    { picklist : Picklist.State
+    { theme : Theme
+    , activeTab : Int
+    , drillDownIsOpen : Bool
+    , picklist : Picklist.State
     , picklistSelection : Maybe Int
     }
 
 
 type Msg
     = DoNothing
+    | SwitchTab Int
+    | OpenDrillDown
+    | CloseDrillDown
     | PicklistChanged Picklist.State
     | PicklistSelectionChanged (Maybe Int)
 
@@ -39,7 +49,10 @@ init =
     let
         components : Components
         components =
-            { picklist = Picklist.init
+            { theme = Theme.fromConfig Nothing
+            , activeTab = 0
+            , drillDownIsOpen = False
+            , picklist = Picklist.init
             , picklistSelection = Nothing
             }
     in
@@ -62,6 +75,21 @@ update msg model =
                 | picklistSelection = maybeSelectedTag
             }
 
+        SwitchTab newActiveTab ->
+            { model
+                | activeTab = newActiveTab
+            }
+
+        OpenDrillDown ->
+            { model
+                | drillDownIsOpen = True
+            }
+
+        CloseDrillDown ->
+            { model
+                | drillDownIsOpen = False
+            }
+
 
 view : Model -> Html Msg
 view model =
@@ -75,22 +103,71 @@ view model =
 viewComponents : Components -> Element Msg
 viewComponents c =
     el [ padding 20 ]
-        (column [ spacing 20 ]
-            [ el [] (text (Debug.toString c.picklist))
-            , el [] (text (Debug.toString c.picklistSelection))
-            , Picklist.view
-                { state = c.picklist
-                , onStateChange = PicklistChanged
-                , selectedTag = c.picklistSelection
-                , onSelectionChange = PicklistSelectionChanged
-                }
-                [ ( 1, text "Option A" )
-                , ( 2, text "Option B" )
-                , ( 3, text "Option C" )
-                , ( 4, text "Option D" )
-                , ( 5, text "Option E" )
-                , ( 6, text "Option F" )
-                , ( 7, text "Option G" )
-                ]
+        (column [ spacing 40 ]
+            [ viewComponent "Tabs"
+                none
+                (text (Debug.toString c.activeTab))
+                (TabsComponent.tabsComponent
+                    { theme = c.theme
+                    , tabs =
+                        Array.fromList
+                            [ { name = "Tab 1"
+                              , content = text "Content 1"
+                              }
+                            , { name = "Tab 2"
+                              , content = text "Content 2"
+                              }
+                            ]
+                    , onSwitchTab = SwitchTab
+                    , activeTab = c.activeTab
+                    }
+                )
+            , viewComponent "Drill-down Panel"
+                none
+                (text (Debug.toString c.drillDownIsOpen))
+                (DrillDownPanel.drillDownPanel
+                    { openMsg = OpenDrillDown
+                    , closeMsg = CloseDrillDown
+                    , depth = 1
+                    , closedElement = text "Closed"
+                    , openHeader = text "Header"
+                    , openElement = text "Content"
+                    , isOpen = c.drillDownIsOpen
+                    }
+                )
+            , viewComponent "Picklist"
+                (text (Debug.toString c.picklist))
+                (text (Debug.toString c.picklistSelection))
+                (Picklist.view
+                    { state = c.picklist
+                    , onStateChange = PicklistChanged
+                    , selectedTag = c.picklistSelection
+                    , onSelectionChange = PicklistSelectionChanged
+                    }
+                    [ ( 1, text "Option A" )
+                    , ( 2, text "Option B" )
+                    , ( 3, text "Option C" )
+                    , ( 4, text "Option D" )
+                    , ( 5, text "Option E" )
+                    , ( 6, text "Option F" )
+                    , ( 7, text "Option G" )
+                    ]
+                )
             ]
         )
+
+
+viewComponent : String -> Element msg -> Element msg -> Element msg -> Element msg
+viewComponent title internalState externalState component =
+    column [ spacing 20 ]
+        [ el [ Font.size 24 ] (text title)
+        , column [ paddingEach { top = 0, right = 0, bottom = 0, left = 20 }, spacing 20 ]
+            [ row [ spacing 10 ] [ text "Internal State:", internalState ]
+            , row [ spacing 10 ] [ text "External State:", externalState ]
+            , el
+                [ padding 20
+                , Background.color (rgb 0.9 0.9 0.9)
+                ]
+                component
+            ]
+        ]
