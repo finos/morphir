888c8c714f87f30413f900af79c038524e7492fd
Kindson The Genius <35315234+KindsonTheGenius@users.noreply.github.com>
2022-11-02 19:28:24 +0000

    Fixed bugs

--- tests/Morphir/JsonSchema/RecordTypesTests.elm
+++ tests/Morphir/JsonSchema/RecordTypesTests.elm
@@ -3,7 +3,7 @@ module Morphir.JsonSchema.RecordTypesTests exposing (..)
 import Dict
 import Expect
 import Morphir.IR.Type as Type
-import Morphir.JsonSchema.AST exposing (SchemaType(..))
+import Morphir.JsonSchema.AST exposing (Derivative(..), SchemaType(..))
 import Morphir.JsonSchema.Backend exposing (mapType)
 import Test exposing (describe, test)
 
@@ -13,7 +13,7 @@ recordTests =
         [ test "Test record with single field" <|
             \_ ->
                 mapType (Type.Record () [ Type.Field [ "firstname" ] (Type.Reference () ( [ [ "Morphir.SDK" ] ], [ [ "String" ] ], [ "string" ] ) []) ])
-                    |> Expect.equal (Ok (Object ([ ( "firstname", String ) ] |> Dict.fromList)))
+                    |> Expect.equal (Ok (Object ([ ( "firstname", String BasicString ) ] |> Dict.fromList)))
         , test "Test for record with two fields" <|
             \_ ->
                 mapType
@@ -22,5 +22,5 @@ recordTests =
                         , Type.Field [ "age" ] (Type.Reference () ( [ [ "Morphir.SDK" ] ], [ [ "Basics" ] ], [ "int" ] ) [])
                         ]
                     )
-                    |> Expect.equal (Ok (Object ([ ( "firstname", String ), ( "age", Integer ) ] |> Dict.fromList)))
+                    |> Expect.equal (Ok (Object ([ ( "firstname", String BasicString ), ( "age", Integer ) ] |> Dict.fromList)))
         ]
