eeac0ee1fe076d96eb3c781f9f9c12adeaa0693b
Kindson The Genius <35315234+KindsonTheGenius@users.noreply.github.com>
2022-11-18 15:50:07 +0000

    Added feature to return all mapping errors

--- src/Morphir/JsonSchema/Backend/Codec.elm
+++ src/Morphir/JsonSchema/Backend/Codec.elm
@@ -1,19 +1,15 @@
 module Morphir.JsonSchema.Backend.Codec exposing (..)
 
-<<<<<<< HEAD
 import Json.Decode as Decode exposing (Decoder)
-import Morphir.JsonSchema.Backend exposing (Options)
+import Json.Encode as Encode
+import Morphir.JsonSchema.Backend exposing (Error, Options)
 
 
 decodeOptions : Decoder Options
 decodeOptions =
     Decode.map Options (Decode.field "filename" Decode.string)
-=======
-import Json.Encode as Encode
-import Morphir.JsonSchema.Backend exposing (Error)
 
 
 encodeError : Error -> Encode.Value
 encodeError error =
     Encode.list Encode.string error
->>>>>>> 031ee9f9 (Added feature to return all mapping errors)
