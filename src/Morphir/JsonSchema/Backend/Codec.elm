b641977558a1a9610afed6c20fe6ac725b31ab8f
Kindson The Genius <35315234+KindsonTheGenius@users.noreply.github.com>
2022-12-01 16:27:17 +0000

    Added modulesToInclude entrypoint to Json Schema bakcend

--- src/Morphir/JsonSchema/Backend/Codec.elm
+++ src/Morphir/JsonSchema/Backend/Codec.elm
@@ -2,12 +2,25 @@ module Morphir.JsonSchema.Backend.Codec exposing (..)
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
+import Morphir.IR.Path as Path
 import Morphir.JsonSchema.Backend exposing (Error, Errors, Options)
+import Set
 
 
 decodeOptions : Decoder Options
 decodeOptions =
-    Decode.map Options (Decode.field "filename" Decode.string)
+    Decode.map2 Options
+        (Decode.field "limitToModules"
+            (Decode.maybe
+                (Decode.list
+                    (Decode.string
+                        |> Decode.map Path.fromString
+                    )
+                    |> Decode.map Set.fromList
+                )
+            )
+        )
+        (Decode.field "filename" Decode.string)
 
 
 encodeErrors : Errors -> Encode.Value
