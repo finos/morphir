afe4824f560a6225c5ab12d7397f3cd1f44f6426
Nwokafor Choongsaeng <nwokafor.choongsaeng@morganstanley.com>
2023-01-11 09:19:27 +0000

    reproduced issue 969 in reference model

---
+++ tests-integration/reference-model/src/Morphir/Reference/Model/Issues/Issue969.elm
@@ -0,0 +1,25 @@
+module Morphir.Reference.Model.Issues.Issue969 exposing (..)
+
+
+identity : a -> a
+identity a =
+    a
+
+
+map : (a -> a) -> a -> a
+map f v =
+    f v
+
+
+useMap : String -> String
+useMap str =
+    map identity str
+
+
+
+-- a more complex case below
+--
+--apply : List (Maybe Float) -> List Float
+--apply strings =
+--    strings
+--        |> List.filterMap identity
