1eb87428c16d5cd901e5325e336e31dce5029036
Osborne Yaw Sapaty <osborne.sapaty@morganstanley.com>
2023-03-14 10:38:32 +0000

    more unit tests for branch coverage

--- tests/Morphir/Correctness/BranchCoverage2Tests.elm
+++ tests/Morphir/Correctness/BranchCoverage2Tests.elm
@@ -2,9 +2,11 @@ module Morphir.Correctness.BranchCoverage2Tests exposing (..)
 
 import Expect
 import Morphir.Correctness.BranchCoverage as BranchCoverage exposing (Condition(..))
+import Morphir.IR.FQName as FQName
 import Morphir.IR.Literal exposing (Literal(..))
 import Morphir.IR.Name as Name
-import Morphir.IR.Value as Value exposing (Value(..))
+import Morphir.IR.Type as Type
+import Morphir.IR.Value as Value exposing (Pattern(..), Value(..))
 import Test exposing (Test, test)
 
 
@@ -23,8 +25,8 @@ simpleIfandElseTest =
             simpleIfandElseValue
                 |> BranchCoverage.valueBranches True
                 |> Expect.equal
-                    [ [ BoolCondition { criterion = less (litInt 5) (litInt 7), expectedValue = True } ]
-                    , [ BoolCondition { criterion = less (litInt 5) (litInt 7), expectedValue = False } ]
+                    [ [ BoolCondition { criterion = less (intLit 5) (intLit 7), expectedValue = True } ]
+                    , [ BoolCondition { criterion = less (intLit 5) (intLit 7), expectedValue = False } ]
                     ]
         )
 
@@ -35,15 +37,15 @@ exampleWithLetDefinition =
         exampleWithLetDefinitionValue : Value () ()
         exampleWithLetDefinitionValue =
             Value.IfThenElse ()
-                (apply3 (basics "equals") (litInt 5) (litInt 7) (litInt 3))
+                (apply3 (basics "equals") (intLit 5) (intLit 7) (intLit 3))
                 (stringLit "All items are equal")
                 (Value.IfThenElse ()
-                    (less (litInt 3) (litInt 5))
+                    (less (intLit 3) (intLit 5))
                     (stringLit "A is greater")
                     (stringLit "C is greater")
                 )
     in
-    test "Testing example with let definition"
+    test "Testing simple If and Else by Substitution from a let"
         (\_ ->
             exampleWithLetDefinitionValue
                 |> BranchCoverage.valueBranches True
@@ -76,11 +78,88 @@ exampleWithLetDefinition =
         )
 
 
+exampleWithPatternMatch : Test
+exampleWithPatternMatch =
+    let
+        value =
+            PatternMatch ()
+                (Variable () [ "HTMLStatusCodes" ])
+                [ ( ConstructorPattern ()
+                        (FQName.fromString "" ".")
+                        [ LiteralPattern () (StringLiteral "InternalServerError") ]
+                  , Value.Tuple () [ intLit 500, stringLit "InternalServerError" ]
+                  )
+                , ( ConstructorPattern ()
+                        (FQName.fromString "" ".")
+                        [ LiteralPattern () (StringLiteral "BadGateway") ]
+                  , Value.Tuple () [ intLit 502, stringLit "BadGateway" ]
+                  )
+                , ( ConstructorPattern ()
+                        (FQName.fromString "" ".")
+                        [ LiteralPattern () (StringLiteral "ServiceUnavailable") ]
+                  , Value.Tuple () [ intLit 503, stringLit "ServiceUnavailable" ]
+                  )
+                , ( ConstructorPattern ()
+                        (FQName.fromString "" ".")
+                        [ LiteralPattern () (StringLiteral "GatewayTimeout") ]
+                  , Value.Tuple () [ intLit 504, stringLit "GatewayTimeout" ]
+                  )
+                ]
+    in
+    test "Testing Simple Pattern Match"
+        (\_ ->
+            value
+                |> BranchCoverage.valueBranches True
+                |> Expect.equal
+                    [ [ PatternCondition
+                            { excludes = []
+                            , includes = ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "InternalServerError") ]
+                            , subject = Variable () [ "HTMLStatusCodes" ]
+                            }
+                      ]
+                    , [ PatternCondition
+                            { excludes =
+                                [ ConstructorPattern
+                                    ()
+                                    ( [ [] ], [], [] )
+                                    [ LiteralPattern () (StringLiteral "InternalServerError") ]
+                                ]
+                            , includes = ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "BadGateway") ]
+                            , subject = Variable () [ "HTMLStatusCodes" ]
+                            }
+                      ]
+                    , [ PatternCondition
+                            { excludes =
+                                [ ConstructorPattern ()
+                                    ( [ [] ], [], [] )
+                                    [ LiteralPattern () (StringLiteral "InternalServerError") ]
+                                , ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "BadGateway") ]
+                                ]
+                            , includes = ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "ServiceUnavailable") ]
+                            , subject = Variable () [ "HTMLStatusCodes" ]
+                            }
+                      ]
+                    , [ PatternCondition
+                            { excludes =
+                                [ ConstructorPattern ()
+                                    ( [ [] ], [], [] )
+                                    [ LiteralPattern
+                                        ()
+                                        (StringLiteral "InternalServerError")
+                                    ]
+                                , ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "BadGateway") ]
+                                , ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "ServiceUnavailable") ]
+                                ]
+                            , includes = ConstructorPattern () ( [ [] ], [], [] ) [ LiteralPattern () (StringLiteral "GatewayTimeout") ]
+                            , subject = Variable () [ "HTMLStatusCodes" ]
+                            }
+                      ]
+                    ]
+        )
 
---exampleWithPatternMatch : Test
---exampleWithPatternMatch =
---    Debug.todo ""
--- TODO:// Import Utility
+
+
+-- TODO: Import Utility from branch coverage
 
 
 apply : Value.Value ta () -> Value.Value ta () -> Value.Value ta ()
@@ -103,8 +182,8 @@ var name =
     Value.Variable () (Name.fromString name)
 
 
-litInt : Int -> Value ta ()
-litInt v =
+intLit : Int -> Value ta ()
+intLit v =
     Value.Literal () (WholeNumberLiteral v)
 
 
@@ -120,7 +199,7 @@ basics localName =
 
 eq : String -> Int -> Value.Value () ()
 eq varName val =
-    apply2 (basics "equal") (var varName) (litInt val)
+    apply2 (basics "equal") (var varName) (intLit val)
 
 
 less : Value.Value () () -> Value.Value () () -> Value.Value () ()
