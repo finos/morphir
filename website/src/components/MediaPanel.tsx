d13a475b51bef8af61005a5a020b17dded03b6e8
thee freelancer <ronaldssebalamu2@gmail.com>
2023-03-22 14:04:00 +0300

    Add table to the Media component

--- website/src/components/MediaPanel.tsx
+++ website/src/components/MediaPanel.tsx
@@ -3,23 +3,22 @@ import React from 'react'
 type MediaList = {
 	Episode: string | JSX.Element
 	Description: JSX.Element
+
 }
 
 const MediaList: MediaList[] = [
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTYx"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/introduction-to-the-morphir-show.jpg" /></a> ',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/introduction-to-the-morphir-show.jpg" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTYx'>
 					Introduction to the Morphir Showcase
-				</a>
 			</>
 		),
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTYz"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/what-morphir-is-with-stephen-gol.jpg" /></a> ',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/what-morphir-is-with-stephen-gol.jpg" />,
 		Description: (
 			<>
 				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTYz'>
@@ -30,54 +29,48 @@ const MediaList: MediaList[] = [
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTY2"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/how-morphir-works-with-attila-mi-1.jpg" /></a>',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/how-morphir-works-with-attila-mi-1.jpg" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTY2'>
 					How Morphir works with Attila Mihaly
-				</a>
 			</>
 		),
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTY4"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/why-morphir-is-important-with-co.jpg" /></a>',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/why-morphir-is-important-with-co.jpg" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTY4'>
 					Why Morphir is Important – with Colin, James & Stephen
-				</a>
 			</>
 		),
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTcw"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/Screenshot-2022-03-02-at-14.35.18.png" /></a>',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/Screenshot-2022-03-02-at-14.35.18.png" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTcw'>
 					The Benefits & Use Case of Morphir with Jane, Chris & Stephen
-				</a>
 			</>
 		),
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTcy"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/how-to-get-involved-closing-pane.jpg" /></a>',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/how-to-get-involved-closing-pane.jpg" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTcy'>
+			
 					How to get involved – Closing Panel Q&A
-				</a>
+			
 			</>
 		),
 	},
 	{
 		Episode:
-			'<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTU5"><img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/morphir-showcase-full-show.jpg" /></a>',
+			<img width="250" src="https://resources.finos.org/wp-content/uploads/2022/03/morphir-showcase-full-show.jpg" />,
 		Description: (
 			<>
-				<a href='https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTU5'>Morphir Showcase – Full Show</a>
+				Morphir Showcase – Full Show
 			</>
 		),
 	},
@@ -95,9 +88,21 @@ export default function MedialPanel(): JSX.Element {
 
 function Media({ ...props }: MediaList) {
 	return (
+		
 		<div>
-			<div>{props.Description}</div>
-			<div>{props.Episode}</div>
+			<a href="https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTU5">
+				<table>
+					<tr>
+						<th>Episode</th>
+						<th>Description</th>
+
+					</tr>
+					<tr>
+						<td>{props.Episode}</td>
+						<td>{props.Description}</td>
+					</tr>
+				</table>
+			</a>
 		</div>
 	)
 }
