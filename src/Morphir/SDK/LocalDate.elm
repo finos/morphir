72a34140d2bd068c668abde7ebb60429eb5b8429
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2023-10-30 22:52:33 +0100

    Added missing functions to LocalDate. Moved Month inside LocalDate (it was unused anyway)

--- src/Morphir/SDK/LocalDate.elm
+++ src/Morphir/SDK/LocalDate.elm
@@ -20,6 +20,9 @@ module Morphir.SDK.LocalDate exposing
     , diffInDays, diffInWeeks, diffInMonths, diffInYears
     , addDays, addWeeks, addMonths, addYears
     , toISOString, fromISO, fromParts
+    , DayOfWeek(..), dayOfWeek, isWeekend, isWeekday
+    , Month(..)
+    , year, month, day
     )
 
 {-| This module adds the definition of a date without time zones. Useful in business modeling.
@@ -40,9 +43,17 @@ module Morphir.SDK.LocalDate exposing
 
 @docs toISOString, fromISO, fromParts
 
+
+# Query
+
+@docs DayOfWeek, dayOfWeek, isWeekend, isWeekday
+@docs Month
+@docs year, month, day
+
 -}
 
 import Date exposing (Date, Unit(..))
+import Time
 
 
 {-| Concept of a date without time zones.
@@ -126,14 +137,14 @@ Errors can occur when any of the given values fall outside of their relevant con
 For example, the date given as 2000 2 30 (2000-Feb-30) would fail because the day of the 30th is impossible.
 -}
 fromParts : Int -> Int -> Int -> Maybe LocalDate
-fromParts year month day =
+fromParts yearNumber monthNumber dayOfMonthNumber =
     -- We do all of this processing because our Elm Date library accepts invalid values while most other languages don't.
     --  So we want to maintain consistency.
     -- Oddly, Date has fromCalendarParts, but it's not exposed.
     let
         maybeMonth =
-            if month > 0 && month < 13 then
-                Just (Date.numberToMonth month)
+            if monthNumber > 0 && monthNumber < 13 then
+                Just (Date.numberToMonth monthNumber)
 
             else
                 Nothing
@@ -141,14 +152,148 @@ fromParts year month day =
     maybeMonth
         |> Maybe.map
             (\m ->
-                ( m, Date.fromCalendarDate year m day )
+                ( m, Date.fromCalendarDate yearNumber m dayOfMonthNumber )
             )
         |> Maybe.map
             (\( dateMonth, date ) ->
-                if Date.year date == year && Date.month date == dateMonth && Date.day date == day then
+                if Date.year date == yearNumber && Date.month date == dateMonth && Date.day date == dayOfMonthNumber then
                     Just date
 
                 else
                     Nothing
             )
         |> Maybe.withDefault Nothing
+
+
+{-| Returns the year as a number.
+-}
+year : LocalDate -> Int
+year localDate =
+    Date.year localDate
+
+
+{-| Returns the month of the year for a given date.
+-}
+month : LocalDate -> Month
+month localDate =
+    case Date.month localDate of
+        Time.Jan ->
+            January
+
+        Time.Feb ->
+            February
+
+        Time.Mar ->
+            March
+
+        Time.Apr ->
+            April
+
+        Time.May ->
+            May
+
+        Time.Jun ->
+            June
+
+        Time.Jul ->
+            July
+
+        Time.Aug ->
+            August
+
+        Time.Sep ->
+            September
+
+        Time.Oct ->
+            October
+
+        Time.Nov ->
+            November
+
+        Time.Dec ->
+            December
+
+
+{-| The day of the month (1–31).
+-}
+day : LocalDate -> Int
+day localDate =
+    Date.day localDate
+
+
+{-| Returns the day of week for a date.
+-}
+dayOfWeek : LocalDate -> DayOfWeek
+dayOfWeek localDate =
+    case Date.weekday localDate of
+        Time.Mon ->
+            Monday
+
+        Time.Tue ->
+            Tuesday
+
+        Time.Wed ->
+            Wednesday
+
+        Time.Thu ->
+            Thursday
+
+        Time.Fri ->
+            Friday
+
+        Time.Sat ->
+            Saturday
+
+        Time.Sun ->
+            Sunday
+
+
+{-| Returns true if the date falls on a weekend (Saturday or Sunday).
+-}
+isWeekend : LocalDate -> Bool
+isWeekend localDate =
+    case dayOfWeek localDate of
+        Saturday ->
+            True
+
+        Sunday ->
+            True
+
+        _ ->
+            False
+
+
+{-| Returns true if the date falls on a weekday (any day other than Saturday or Sunday).
+-}
+isWeekday : LocalDate -> Bool
+isWeekday localDate =
+    not (isWeekend localDate)
+
+
+{-| Type that represents a day of the week.
+-}
+type DayOfWeek
+    = Monday
+    | Tuesday
+    | Wednesday
+    | Thursday
+    | Friday
+    | Saturday
+    | Sunday
+
+
+{-| Gregorian calendar months in English.
+-}
+type Month
+    = January
+    | February
+    | March
+    | April
+    | May
+    | June
+    | July
+    | August
+    | September
+    | October
+    | November
+    | December
