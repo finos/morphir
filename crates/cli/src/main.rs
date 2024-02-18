4958f76155e5ec2408156937844d448fbd472a07
Paul Folbrecht <paul.folbrecht@gmail.com>
2024-02-18 13:19:23 -0600

    help messages

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -20,10 +20,13 @@ enum Commands {
 #[derive(Debug, Args)]
 #[command(args_conflicts_with_subcommands = true)]
 #[command(flatten_help = true)]
+#[command(about = "Translate Elm sources to Morphir IR")]
 struct MakeArgs {
     #[arg(short, long)]
+    /// Root directory of the project where morphir.json is located. (default: ".")
     project_dir: Option<OsString>,
     #[arg(short, long)]
+    /// Target file location where the Morphir IR will be saved. (default: "morphir-ir.json")
     output: Option<OsString>,
 }
 
@@ -32,14 +35,19 @@ struct MakeArgs {
 #[command(flatten_help = true)]
 struct GenArgs {
     #[arg(short, long)]
+    /// Source location where the Morphir IR will be loaded from. (default: "morphir-ir.json")
     input: Option<OsString>,
     #[arg(short, long)]
+    /// Target location where the generated code will be saved. (default: "./dist")
     output: Option<OsString>,
     #[arg(short, long)]
+    /// Language to Generate (Scala | SpringBoot | cypher | triples). (default: "Scala")
     target: Option<OsString>,
     #[arg(short = 'v', long)]
+    /// Language version to Generate. (default: "2.11")
     target_version: Option<OsString>,
     #[arg(short, long)]
+    /// Copy the dependencies used by the generated code to the output path. (default: false)
     copy_deps: Option<OsString>,
 }
 
@@ -48,6 +56,7 @@ struct GenArgs {
 #[command(flatten_help = true)]
 struct DevelopArgs {
     #[arg(short, long)]
+    /// Root directory of the project where morphir.json is located. (default: ".")
     project_dir: Option<OsString>,
 }
 
