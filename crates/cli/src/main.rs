e77855cc7df7f37d9dcae57e7c8d1898c0c4378e
Paul Folbrecht <paul.folbrecht@gmail.com>
2024-02-20 19:01:42 -0600

    flushed-out commands and args, etc.

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -12,30 +12,73 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     Make(MakeArgs),
-    Restore {},
+    Gen(GenArgs),
+    Develop(DevelopArgs),
+    Restore,
 }
 
 #[derive(Debug, Args)]
 #[command(args_conflicts_with_subcommands = true)]
 #[command(flatten_help = true)]
+#[command(about = "Translate Elm sources to Morphir IR")]
 struct MakeArgs {
     #[arg(short, long)]
+    /// Root directory of the project where morphir.json is located. (default: ".")
+    project_dir: Option<OsString>,
+    #[arg(short, long)]
+    /// Target file location where the Morphir IR will be saved. (default: "morphir-ir.json")
     output: Option<OsString>,
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+#[command(about = "Generate code from Morphir IR")]
+struct GenArgs {
+    #[arg(short, long)]
+    /// Source location where the Morphir IR will be loaded from. (default: "morphir-ir.json")
+    input: Option<OsString>,
+    #[arg(short, long)]
+    /// Target location where the generated code will be saved. (default: "./dist")
+    output: Option<OsString>,
+    #[arg(short, long)]
+    /// Language to Generate (Scala | SpringBoot | cypher | triples). (default: "Scala")
+    target: Option<OsString>,
+    #[arg(short = 'v', long)]
+    /// Language version to Generate. (default: "2.11")
+    target_version: Option<OsString>,
+    #[arg(short, long)]
+    /// Copy the dependencies used by the generated code to the output path. (default: false)
+    copy_deps: Option<OsString>,
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+#[command(about = "Start up a web server and expose developer tools through a web UI")]
+struct DevelopArgs {
+    #[arg(short, long)]
+    /// Root directory of the project where morphir.json is located. (default: ".")
+    project_dir: Option<OsString>,
 }
 
 fn main() {
     let args = Cli::parse();
     match args.command {
         Commands::Make(args) => {
-            println!("Making...");
+            println!("Make - Not Implemented Yet");
             println!("Args: {:?}", args);
-            println!("NOT IMPLEMENTED YET!");
-            println!("Done!");
         }
-        Commands::Restore {} => {
-            println!("Restoring...");
-            println!("NOT IMPLEMENTED YET!");
-            println!("Done!");
+        Commands::Gen(args) => {
+            println!("Generating - Not Implemented Yet");
+            println!("Args: {:?}", args);
+        }
+        Commands::Develop(args) => {
+            println!("Develop (Starting Server) - Not Implemented Yet");
+            println!("Args: {:?}", args);
+        }
+        Commands::Restore => {
+            println!("Restoring - Not Implemented Yet");
         }
     }
 }
