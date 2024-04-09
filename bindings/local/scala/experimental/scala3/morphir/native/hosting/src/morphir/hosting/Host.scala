69c6dbe4f96613b6a889bb3926aadba9c21e7468
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-09 16:33:45 -0400

    Added full JNA usage example

--- bindings/local/scala/experimental/scala3/morphir/native/hosting/src/morphir/hosting/Host.scala
+++ bindings/local/scala/experimental/scala3/morphir/native/hosting/src/morphir/hosting/Host.scala
@@ -1,9 +1,15 @@
 package morphir.hosting 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+
 object hosting {
-    
+    def main(args: Array[String]): Unit = {
+        val lib = Native.load("c", classOf[CMath]).asInstanceOf[CMath]
+        println(lib.cosh(0))
+    }
 }
 
 
-trait CMath extends Library {}
+trait CMath extends Library {
+    def cosh(value:Double):Double
+}
