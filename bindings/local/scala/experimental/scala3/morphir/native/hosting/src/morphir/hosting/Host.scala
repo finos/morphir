875522594f8aa7703095aa9043675c0900f6db07
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-09 17:31:11 -0400

    Add Platform helper class

--- bindings/local/scala/experimental/scala3/morphir/native/hosting/src/morphir/hosting/Host.scala
+++ bindings/local/scala/experimental/scala3/morphir/native/hosting/src/morphir/hosting/Host.scala
@@ -1,6 +1,7 @@
 package morphir.hosting 
-import com.sun.jna.Library;
-import com.sun.jna.Native;
+import com.sun.jna.Library
+import com.sun.jna.Native
+
 
 object hosting {
     def main(args: Array[String]): Unit = {
@@ -12,4 +13,12 @@ object hosting {
 
 trait CMath extends Library {
     def cosh(value:Double):Double
+}
+
+object Platform {
+    import com.sun.jna.{Platform as JNAPlatform}
+
+    def ifWindows[T](onWindows: => T, onOther: => T):T = {
+        if(JNAPlatform.isWindows()) onWindows else onOther
+    }
 }
