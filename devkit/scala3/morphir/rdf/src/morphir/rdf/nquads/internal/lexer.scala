24ac86c0c61e183689715f012828c1fe4d9721f8
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-17 14:42:04 -0400

    Add PN_CHARS related nonterminals

--- devkit/scala3/morphir/rdf/src/morphir/rdf/nquads/internal/lexer.scala
+++ devkit/scala3/morphir/rdf/src/morphir/rdf/nquads/internal/lexer.scala
@@ -1,7 +1,7 @@
 package morphir.rdf.nquads.internal
 import parsley.Parsley.{atomic, many, some}
 import parsley.character.{endOfLine, letter, hexDigit, oneOf, stringOfSome}
-import parsley.unicode.{char as uchar, oneOf as oneOfUnicode}
+import parsley.unicode.{char as uchar, oneOf as oneOfUnicode, noneOf as noneOfUnicode}
 import parsley.syntax.character.{charLift, stringLift}
 import parsley.expr.chain
 
@@ -21,16 +21,20 @@ object lexer:
 
   lazy val UCHAR = 
     val uchar4 =
-      ('\\' ~> 'u' ~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit).map {
-        case ((((a, b)), c), d) => s"\\u${a}${b}${c}${d}"
-      }
+      ('\\' ~> 'u' ~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit).span
     val uchar8 =
-      ('\\' ~> 'U' ~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit)
-        .map { case (((((((((a, b)), c), d), e), f), g), h)) =>
-          s"\\U${a}${b}${c}${d}${e}${f}${g}${h}"
-        }
+      ('\\' ~> 'U' ~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit <~> hexDigit).span
     uchar4 | uchar8
   end UCHAR 
+
+  lazy val ECHAR = '\\' 
+
+  lazy val PN_CHARS_BASE =
+    unicode.upper | unicode.letter | oneOfUnicode(0x00C0 to 0x00D6) | oneOfUnicode(0x00D8 to 0x00F6) | oneOfUnicode(0x00F8 to 0x02FF) | oneOfUnicode(0x0370 to 0x037D) | oneOfUnicode(0x037F to 0x1FFF) | oneOfUnicode(0x200C to 0x200D) | oneOfUnicode(0x2070 to 0x218F) | oneOfUnicode(0x2C00 to 0x2FEF) | oneOfUnicode(0x3001 to 0xD7FF) | oneOfUnicode(0xF900 to 0xFDCF) | oneOfUnicode(0xFDF0 to 0xFFFD) | oneOfUnicode(0x10000 to 0xEFFFF)
+
+  lazy val PN_CHARS_U = PN_CHARS_BASE | unicode.underscore | unicode.colon
+
+  lazy val PN_CHARS = PN_CHARS_U | unicode.minus | unicode.digit | unicode.middot | oneOfUnicode(0x0300 to 0x036F) | oneOfUnicode(0x203F to 0x2040)
 
   object ascii:
     val lowercaseLetter = oneOf('a' to 'z')
@@ -47,5 +51,9 @@ object lexer:
     val letter = lower | upper
     val digit = oneOf(0x0030 to 0x0039)
     val alphaNumeric = lower | upper | digit
+    val underscore = uchar(0x005F)
+    val colon = uchar(0x003A)
+    val minus = uchar(0x002D)
+    val middot = uchar(0x00B7)
   end unicode
 end lexer
