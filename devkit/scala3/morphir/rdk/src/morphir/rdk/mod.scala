0c592f4dc3c3ca034b59d698c77f9559e066106a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-18 17:36:49 -0400

    Fix build error

--- devkit/scala3/morphir/rdk/src/morphir/rdk/mod.scala
+++ devkit/scala3/morphir/rdk/src/morphir/rdk/mod.scala
@@ -1,6 +1,7 @@
 package morphir.rdk
 import morphir.runtime.internal.*
 import izumi.reflect.*
+import morphir.cdk.IntoName
 
 extension [Self: IntoName](self: Self)
   def intoVariable[V](using tag: Tag[V]): Symbol[V] =
