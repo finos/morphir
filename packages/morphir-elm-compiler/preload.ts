48e836a19d6cd1732cae6672f1a84a740ddc079a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-05-26 16:26:25 -0400

    Delete un-needed attempt at a Bun Elm plugin (figured out proper vite config)

--- packages/morphir-elm-compiler/preload.ts
+++ packages/morphir-elm-compiler/preload.ts
@@ -1,9 +1,5 @@
 import { plugin } from "bun";
-import { ElmPlugin } from "bun-elm-plugin";
 
 console.log("Preload is running...");
-
-console.log("Registering Elm plugin...");
-//plugin(ElmPlugin());
 
 console.log("Preload is done.");
