56fbb12906b2e45345d23e653d483ba5449a9271
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-09-03 16:08:17 -0500

    Added additional methods to the System service

--- morphir/src/org/finos/morphir/services/System.scala
+++ morphir/src/org/finos/morphir/services/System.scala
@@ -5,6 +5,8 @@ import kyo.{Path as _, *}
 
 trait System:
     def env(variable: => String): Option[String] < (IO & Abort[SecurityException])        
+    def envOrElse(variable: => String, alt: => String): String < (IO & Abort[SecurityException])
+    def envOrOption(variable: => String, alt: => Option[String]): Option[String] < (IO & Abort[SecurityException])
     def envs: Map[String, String] < (IO & Abort[SecurityException])
 
 object System:
@@ -12,10 +14,16 @@ object System:
         Layer {
             SystemLive
         }
-        
+
     object SystemLive extends System:
         def env(variable: => String): Option[String] < (IO & Abort[SecurityException]) =
             val effect = IO(sys.env.get(variable))
+            Abort.catching[SecurityException](effect)        
+        def envOrElse(variable: => String, alt: => String): String < (IO & Abort[SecurityException]) = 
+            val effect = IO(sys.env.getOrElse(variable, alt))
+            Abort.catching[SecurityException](effect)
+        def envOrOption(variable: => String, alt: => Option[String]): Option[String] < (IO & Abort[SecurityException]) =
+            val effect = IO(sys.env.get(variable).orElse(alt))
             Abort.catching[SecurityException](effect)
         def envs: Map[String, String] < (IO & Abort[SecurityException]) = 
             val effect = IO(sys.env)
