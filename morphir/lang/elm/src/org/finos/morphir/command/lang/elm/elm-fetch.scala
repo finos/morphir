998803099f87354e8789489c48270a5d381aab90
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-09-06 18:59:43 -0500

    Change message

--- morphir/lang/elm/src/org/finos/morphir/command/lang/elm/elm-fetch.scala
+++ morphir/lang/elm/src/org/finos/morphir/command/lang/elm/elm-fetch.scala
@@ -9,6 +9,6 @@ final case class FetchParams(projectDir: kyo.Path)
 final case class Fetch() extends Command[FetchParams]:
   def run(params: FetchParams): Unit < (IO & Abort[Throwable]) =
     defer {
-      await(Console.println("Running fetch"))
+      await(Console.println("Running fetch..."))
       await(Console.println("Fetch Params", pprint(params)))
     }
