a90987651ff3006b7cbbc68c387255654548c3e4
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-09-11 08:54:06 -0500

    Add commands for the develop command

--- morphir/cli/src/org/finos/morphir/cli/command/develop.scala
+++ morphir/cli/src/org/finos/morphir/cli/command/develop.scala
@@ -1,9 +1,20 @@
 package org.finos.morphir.cli.command
-
+import org.finos.morphir.*
 import caseapp.*
 import org.finos.morphir.cli.given
 
-final case class DevelopOptions()
+@HelpMessage("Start up a web server and expose developer tools through a web UI.")
+final case class DevelopOptions(
+    @Name("p")
+    @HelpMessage("Port to bind the web server to. (default: 3000)")
+    port:Int = 3000,
+    @Name("o")
+    @HelpMessage("Host to bind the web server to. (default: localhost)")
+    host:String = "localhost",
+    @Name("i")
+    @HelpMessage("Root directory of the project where morphir.json is located. (default: .)")
+    projectDir:GenericPath = Path.parse(os.pwd.toString)
+)
 
 object Develop extends Command[DevelopOptions]:  
   def run(options: DevelopOptions, remainingArgs: RemainingArgs): Unit =
