2db4fa3ef56bb692924d6fc524a7c0c11bece357
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-09-12 14:14:57 -0500

    Add support for ToConfigured

--- morphir/test/src/org/finos/morphir/config/ConfigSpec.scala
+++ morphir/test/src/org/finos/morphir/config/ConfigSpec.scala
@@ -3,9 +3,10 @@ import kyo.*
 import org.finos.morphir.testing.*
 import metaconfig.{pprint as _, *}
 import metaconfig.sconfig.*
-import zio.test.*
+import zio.test.{Result as _, *}
 object ConfigSpec extends MorphirSpecDefault:
   def spec = suite("ConfigSpec")(
+    preludeSuite,
     workspaceMembersSuite
   )
 
@@ -22,4 +23,53 @@ object ConfigSpec extends MorphirSpecDefault:
         result.value.get.workspaceProjects == IndexedSeq("common", "project-01", "project-02")
       )
     }
+  )
+
+  def preludeSuite = suite("Prelude")(
+    test("Should be able to convert an Either[Throwable, A] to a Configured[A] with a success result") {
+      val result: Either[Throwable, Int] = Right(42)
+      val configured: Configured[Int]    = result.toConfigured()
+      assertTrue(configured == Configured.Ok(42))
+    },
+    test("Should be able to convert an Either[Throwable, A] to a Configured[A] with a failure result") {
+      val result: Either[Throwable, Int] = Left(new Throwable("An error occurred"))
+      val configured: Configured[Int]    = result.toConfigured()
+      assertTrue(configured == Configured.exception(new Throwable("An error occurred")))
+    },
+    test("Should be able to convert an Either[String, A] to a Configured[A] with a success result") {
+      val result: Either[String, Int] = Right(42)
+      val configured: Configured[Int] = result.toConfigured()
+      assertTrue(configured == Configured.Ok(42))
+    },
+    test("Should be able to convert an Either[String, A] to a Configured[A] with a failure result") {
+      val result: Either[String, Int] = Left("An error occurred")
+      val configured: Configured[Int] = result.toConfigured()
+      assertTrue(configured == Configured.error("An error occurred"))
+    },
+    test("Should be able to convert a Result[Throwable, A] to a Configured[A] with a success result") {
+      val result: Result[Throwable, Int] = Result.success(42)
+      val configured: Configured[Int]    = result.toConfigured()
+      assertTrue(configured == Configured.Ok(42))
+    },
+    test("Should be able to convert a Result[Throwable, A] to a Configured[A] with a failure result") {
+      val result: Result[Throwable, Int] = Result.fail(new Throwable("An error occurred"))
+      val configured: Configured[Int]    = result.toConfigured()
+      assertTrue(configured == Configured.exception(new Throwable("An error occurred")))
+    },
+    test("Should be able to convert a Result[Throwable, A] with a panic to a Configured[A] with a failure result") {
+      val result: Result[Throwable, Int] = Result.panic(new Throwable("Oh No!!!!"))
+      val configured: Configured[Int]    = result.toConfigured()
+      assertTrue(configured == Configured.exception(new Throwable("Oh No!!!!")))
+    },
+    test("Should be able to convert a Result[String, A] to a Configured[A] with a success result") {
+      val result: Result[String, Int] = Result.success(42)
+      val configured: Configured[Int] = result.toConfigured()
+      assertTrue(configured == Configured.Ok(42))
+    },
+    test("Should be able to convert a Result[String, A] to a Configured[A] with a failure result") {
+      val result: Result[String, Int] = Result.fail("An error occurred")
+      val configured: Configured[Int] = result.toConfigured()
+      assertTrue(configured == Configured.error("An error occurred"))
+    },   
+
   )
