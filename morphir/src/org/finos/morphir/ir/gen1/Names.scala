3ccc0c31f069fb2e84b478f7cc2ca5de770fb983
MichelleChan <michelle.chan@capitalone.com>
2024-10-03 15:48:02 -0400

    Adding more IR files

---
+++ morphir/src/org/finos/morphir/ir/gen1/Names.scala
@@ -0,0 +1,13 @@
+package org.finos.morphir.ir.gen1
+
+trait Names
+    extends FQNameModule
+    with ModuleNameModule
+    with NameModule
+    with NamespaceModule
+    with NodeIDModule
+    with PathModule
+    with PackageNameModule
+    with QualifiedModuleNameModule
+    with QNameModule
+    with NamingOptionsModule
