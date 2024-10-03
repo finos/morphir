6b7f761ab8232b1b125aa88df9b7744c94937672
MichelleChan <6752211+michelchan@users.noreply.github.com>
2024-10-03 19:57:26 -0400

    Changing to neotype

--- morphir/src/org/finos/morphir/ir/gen1/name.scala
+++ morphir/src/org/finos/morphir/ir/gen1/name.scala
@@ -1,94 +1,14 @@
 package org.finos.morphir.ir.gen1
 
 import scala.annotation.tailrec
+import neotype.*
 
 /** `Name` is an abstraction of human-readable identifiers made up of words. This abstraction allows us to use the same
   * identifiers across various naming conventions used by the different frontend and backend languages Morphir
   * integrates with.
   */
-sealed case class Name private (toList: List[String]) {
-  self =>
-  // def :+(that: String): Name = Name(self.toList :+ that)
-
-  // def +:(that: String): Name = Name(that +: self.toList)
-
-  def ++(that: Name): Name = Name(self.toList ++ that.toList)
-
-  def +(that: String): Name = self ++ Name.fromString(that)
-
-  // def /(that: Name): Path = Path(IndexedSeq(self, that))
-
-  def humanize: List[String] = {
-    val words                        = toList
-    val join: List[String] => String = abbrev => abbrev.map(_.toUpperCase()).mkString("")
-
-    @tailrec
-    def loop(
-      prefix: List[String],
-      abbrev: List[String],
-      suffix: List[String]
-    ): List[String] =
-      suffix match {
-        case Nil =>
-          abbrev match {
-            case Nil => prefix
-            case _   => prefix ++ List(join(abbrev))
-          }
-        case first :: rest =>
-          if (first.length() == 1)
-            loop(prefix, abbrev ++ List(first), rest)
-          else
-            abbrev match {
-              case Nil => loop(prefix ++ List(first), List.empty, rest)
-              case _ =>
-                loop(prefix ++ List(join(abbrev), first), List.empty, rest)
-            }
-      }
-
-    loop(List.empty, List.empty, words.toList)
-  }
-
-  /** Maps segments of the `Name`.
-    */
-  def mapParts(f: String => String): Name = Name(self.toList.map(f))
-
-  def mkString(f: String => String)(sep: String): String =
-    toList.map(f).mkString(sep)
-
-  def render(implicit renderer: NameRenderer): String = renderer.render(self)
-
-  def toUpperCase: String = mkString(part => part.toUpperCase)("")
-
-  // def toLocalName(implicit renderer: Name.Renderer): LocalName = {
-  //   val localNameStr = render
-  //   LocalName(localNameStr)
-  // }
-
-  def toLowerCase: String =
-    mkString(part => part.toLowerCase)("")
-
-  def toCamelCase: String =
-    toList match {
-      case Nil => ""
-      case head :: tail =>
-        (head :: tail.map(_.capitalize)).mkString("")
-    }
-
-  def toKebabCase: String =
-    humanize.mkString("-")
-
-  def toSnakeCase: String =
-    humanize.mkString("_")
-
-  def toTitleCase: String =
-    toList
-      .map(_.capitalize)
-      .mkString("")
-
-  override def toString: String = toList.mkString("[", ",", "]")
-}
-
-object Name {
+type Name = Name.type
+object Name extends Newtype[List[String]] {
 
   val empty: Name = Name(Nil)
 
@@ -119,16 +39,6 @@ object Name {
   def fromString(str: String): Name =
     Name(pattern.findAllIn(str).toList.map(_.toLowerCase()))
 
-  def toList(name: Name): List[String] = name.toList
-
-  @inline def toTitleCase(name: Name): String = name.toTitleCase
-
-  @inline def toCamelCase(name: Name): String = name.toCamelCase
-
-  @inline def toSnakeCase(name: Name): String = name.toSnakeCase
-
-  @inline def toKebabCase(name: Name): String = name.toKebabCase
-
   @inline def toHumanWords(name: Name): List[String] = name.humanize
 
   object VariableName {
@@ -136,6 +46,86 @@ object Name {
       Some(name.toCamelCase)
   }
 
+  extension (self: Name)
+    def value: List[String] = self
+    // def :+(that: String): Name = Name(self.toList :+ that)
+
+    // def +:(that: String): Name = Name(that +: self.toList)
+
+    def ++(that: Name): Name = Name(value ++ that)
+
+    def +(that: String): Name = value ++ Name.fromString(that)
+
+    // def /(that: Name): Path = Path(IndexedSeq(self, that))
+
+    def humanize: List[String] = {
+      val words                        = toList
+      val join: List[String] => String = abbrev => abbrev.map(_.toUpperCase()).mkString("")
+
+      @tailrec
+      def loop(
+        prefix: List[String],
+        abbrev: List[String],
+        suffix: List[String]
+      ): List[String] =
+        suffix match {
+          case Nil =>
+            abbrev match {
+              case Nil => prefix
+              case _   => prefix ++ List(join(abbrev))
+            }
+          case first :: rest =>
+            if (first.length() == 1)
+              loop(prefix, abbrev ++ List(first), rest)
+            else
+              abbrev match {
+                case Nil => loop(prefix ++ List(first), List.empty, rest)
+                case _ =>
+                  loop(prefix ++ List(join(abbrev), first), List.empty, rest)
+              }
+        }
+
+      loop(List.empty, List.empty, words.toList)
+    }
+
+    /** Maps segments of the `Name`.
+      */
+    def mapParts(f: String => String): Name = Name(value.map(f))
+
+    def mkString(f: String => String)(sep: String): String =
+      value.map(f).mkString(sep)
+
+    def render(implicit renderer: NameRenderer): String = renderer.render(self)
+
+    def toUpperCase: String = mkString(part => part.toUpperCase)("")
+
+    // def toLocalName(implicit renderer: Name.Renderer): LocalName = {
+    //   val localNameStr = render
+    //   LocalName(localNameStr)
+    // }
+
+    def toLowerCase: String =
+      mkString(part => part.toLowerCase)("")
+
+    def toCamelCase: String =
+      value match {
+        case Nil => ""
+        case head :: tail =>
+          (head :: tail.map(_.capitalize)).mkString("")
+      }
+
+    def toKebabCase: String =
+      humanize.mkString("-")
+
+    def toSnakeCase: String =
+      humanize.mkString("_")
+
+    def toTitleCase: String =
+      value
+        .map(_.capitalize)
+        .mkString("")
+
+    override def toString: String = value.mkString("[", ",", "]")
 }
 
 trait NameRenderer extends (Name => String) {
