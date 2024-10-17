f4218286e502a2713bc4125d7389480128ad8ef9
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-10-17 17:23:27 -0500

    A little more reorg of the SDK module

--- bindings/go/morphir/sdk/maybe/map.go
+++ bindings/go/morphir/sdk/maybe/map.go
@@ -15,3 +15,14 @@ func MapUncurried[T any, U any](from Maybe[T], mapper func(T) U) Maybe[U] {
 	}
 	return New[U](mapper(from.value))
 }
+
+func Map2[A any, B any, Value any](mapper func(A) func(B) Value) func(Maybe[A]) func(Maybe[B]) Maybe[Value] {
+	return func(a Maybe[A]) func(Maybe[B]) Maybe[Value] {
+		return func(b Maybe[B]) Maybe[Value] {
+			if a.IsEmpty() || b.IsEmpty() || mapper == nil {
+				return Nothing[Value]()
+			}
+			return New[Value](mapper(a.value)(b.value))
+		}
+	}
+}
