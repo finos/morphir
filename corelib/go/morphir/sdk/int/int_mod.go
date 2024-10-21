c456a7ef33e2c55f236bb232a67e19edbd3fece8
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-10-21 14:52:04 -0500

    Use math module to provide the appropriate constants (#329)

--- corelib/go/morphir/sdk/int/int_mod.go
+++ corelib/go/morphir/sdk/int/int_mod.go
@@ -3,6 +3,7 @@ package int
 import (
 	"github.com/finos/morphir/corelib/go/morphir/sdk/basics"
 	"github.com/finos/morphir/corelib/go/morphir/sdk/maybe"
+	"math"
 )
 
 type Int8 int8
@@ -30,28 +31,28 @@ func FromInt64[I64 ~int64](i I64) basics.Int {
 }
 
 func ToInt8(i basics.Int) maybe.Maybe[Int8] {
-	if i < -128 || i > 127 {
+	if i < math.MinInt8 || i > math.MaxInt8 {
 		return maybe.Nothing[Int8]()
 	}
 	return maybe.Just(Int8(i))
 }
 
 func ToInt16(i basics.Int) maybe.Maybe[Int16] {
-	if i < -32768 || i > 32767 {
+	if i < math.MinInt16 || i > math.MaxInt16 {
 		return maybe.Nothing[Int16]()
 	}
 	return maybe.Just(Int16(i))
 }
 
 func ToInt32(i basics.Int) maybe.Maybe[Int32] {
-	if i < -2147483648 || i > 2147483647 {
+	if i < math.MinInt32 || i > math.MaxInt32 {
 		return maybe.Nothing[Int32]()
 	}
 	return maybe.Just(Int32(i))
 }
 
 func ToInt64(i basics.Int) maybe.Maybe[Int64] {
-	if i < -9223372036854775808 || i > 9223372036854775807 {
+	if i < math.MinInt64 || i > math.MaxInt64 {
 		return maybe.Nothing[Int64]()
 	}
 	return maybe.Just(Int64(i))
