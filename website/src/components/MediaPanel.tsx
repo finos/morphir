a4326ff694778bcf82a0519f196d3f0a749148e1
Juan Estrella <juan.estrella@finos.org>
2025-11-05 11:10:46 +0100

    clean up media links

--- website/src/components/MediaPanel.tsx
+++ website/src/components/MediaPanel.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect } from 'react'
 import Styles from './MediaPanel.module.css'
 
 type MediaList = {
-	EpisodeImg: string
 	EpisodeUrl: string
 	Description: string
 	thumbnailTime?: number // Time in seconds for thumbnail frame (optional)
@@ -10,46 +9,39 @@ type MediaList = {
 
 const MediaList: MediaList[] = [
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/morphir_-_updated_vo_%5Bv3%5D%20(1080p).mp4',
-		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/morphir_-_updated_vo_%5Bv3%5D%20(1080p).mp4',
+		EpisodeUrl: 'https://www.finos.org/hubfs/Projects%20%2B%20SIGs/Morphir/Morphir%20Resource%20Page%202025/morphir_case_study_-_introduction_%5Bv1%5D%20(1080p).mp4',
 		Description: 'Introduction to the Morphir Showcase',
 		thumbnailTime: 5, // Set thumbnail time in seconds (e.g., 5 seconds into the video)
 	},
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/what_morphir_is_with_stephen_-_morphir_showcase_v2%20(1080p).mp4',
 		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/what_morphir_is_with_stephen_-_morphir_showcase_v2%20(1080p).mp4',
 		Description: 'What Morphir is with Stephen Goldbaum',
 		thumbnailTime: 21, // Set thumbnail time in seconds
 	},
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/how_morphir_works_with_attila_-_morphir_showcase_v1%20(1080p).mp4',
 		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/how_morphir_works_with_attila_-_morphir_showcase_v1%20(1080p).mp4',
 		Description: 'How Morphir works with Attila Mihaly',
 		thumbnailTime: 19, // Set thumbnail time in seconds
 	},
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/why_morphir_is_important_-_morphir_showcase%20(1080p).mp4',
 		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/why_morphir_is_important_-_morphir_showcase%20(1080p).mp4',
 		Description: 'Why Morphir is Important – with Colin, James & Stephen',
 		thumbnailTime: 22, // Set thumbnail time in seconds
 	},
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/the_benefits_%26_use_case_-_morphir_showcase%20(1080p).mp4',
 		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/the_benefits_%26_use_case_-_morphir_showcase%20(1080p).mp4',
 		Description: 'The Benefits & Use Case of Morphir with Jane, Chris & Stephen',
 		thumbnailTime: 18, // Set thumbnail time in seconds
 	},
 	{
-		EpisodeImg: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/how_to_get_involved_-_closing_panel_q%26a_-_morphir_showcase%20(1080p).mp4',
 		EpisodeUrl: 'https://www.finos.org/hubfs/2025/Morphir%20Resource%20Page%202025/how_to_get_involved_-_closing_panel_q%26a_-_morphir_showcase%20(1080p).mp4',
 		Description: 'How to get involved – Closing Panel Q&A',
-		thumbnailTime: 7, // Set thumbnail time in seconds
+		thumbnailTime: 300, // Set thumbnail time in seconds
 	},
 	{
-		EpisodeImg: 'https://resources.finos.org/wp-content/uploads/2022/03/morphir-showcase-full-show.jpg',
-		EpisodeUrl: 'https://resources.finos.org/znglist/morphir-showcase/?c=cG9zdDoxNTU5',
+		EpisodeUrl: 'https://www.finos.org/hubfs/Projects%20%2B%20SIGs/Morphir/Morphir%20Resource%20Page%202025/morphir_showcase_-_full_show%20(1080p).mp4',
 		Description: 'Morphir Showcase – Full Show',
-		thumbnailTime: 8, // Set thumbnail time in seconds
+		thumbnailTime: 2, // Set thumbnail time in seconds
 	},
 ]
 
@@ -108,14 +100,14 @@ export default function MediaPanel (): JSX.Element{
 				<div className="col col--4" key={idx}>
 					<div className={Styles.videoContainer}>
 						<a href={props.EpisodeUrl}> 
-							{props.EpisodeImg.endsWith('.mp4') ? (
+							{props.EpisodeUrl.endsWith('.mp4') ? (
 								<VideoThumbnail 
-									src={props.EpisodeImg}
+									src={props.EpisodeUrl}
 									thumbnailTime={props.thumbnailTime}
 									description={props.Description}
 								/>
 							) : (
-								<img src={props.EpisodeImg} alt={props.Description}></img>
+								<img src={props.EpisodeUrl} alt={props.Description}></img>
 							)}
 						</a>
 						<br/>
