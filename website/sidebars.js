f867380f4b15217d156a9d8349f60fbb1f6e64a0
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2026-01-30 13:16:05 -0600

    fix: correct tutorials document ID in sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -37,7 +37,7 @@ const sidebars = {
             'getting-started/editor-setup',
           ],
         },
-        'getting-started/tutorials',
+        'getting-started/morphir-quick-start',
         'getting-started/Morphir Web UI',
         'getting-started/morphir-go-preview',
       ],
